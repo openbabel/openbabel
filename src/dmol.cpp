2ef274d8aaed5e50d7f78b82d14e9f0f574dd391
Geoffrey Hutchison <geoff@geoffhutchison.net>
2003-03-14 18:22:37 +0000

    (ReadDMol): Read $cell vector section if it exists. (WriteDMol): Write unit cell data if it exists.

--- src/dmol.cpp
+++ src/dmol.cpp
@@ -26,13 +26,42 @@ bool ReadDMol(istream &ifs,OBMol &mol,const char *title)
   string str;
   float x,y,z;
   OBAtom *atom;
+  vector3 v1,v2,v3;
   vector<string> vs;
 
   ifs.getline(buffer,BUFF_SIZE);
-  while (strstr(buffer,"$coordinates") == NULL)
+  while (strstr(buffer,"$coordinates") == NULL &&
+	 strstr(buffer,"$cell vectors") == NULL)
     ifs.getline(buffer,BUFF_SIZE);
 
-  //  ifs.getline(buffer,BUFF_SIZE);
+  if (strstr(buffer,"$cell vectors") != NULL)
+    {
+      ifs.getline(buffer,BUFF_SIZE);
+      tokenize(vs,buffer); // we really need to check that it's 3 entries only
+      x = atof((char*)vs[0].c_str()) * BOHR_TO_ANGSTROM;
+      y = atof((char*)vs[1].c_str()) * BOHR_TO_ANGSTROM;
+      z = atof((char*)vs[2].c_str()) * BOHR_TO_ANGSTROM;
+      v1.Set(x,y,z);
+      ifs.getline(buffer,BUFF_SIZE);
+      tokenize(vs,buffer);
+      x = atof((char*)vs[0].c_str()) * BOHR_TO_ANGSTROM;
+      y = atof((char*)vs[1].c_str()) * BOHR_TO_ANGSTROM;
+      z = atof((char*)vs[2].c_str()) * BOHR_TO_ANGSTROM;
+      v2.Set(x,y,z);
+      ifs.getline(buffer,BUFF_SIZE);
+      tokenize(vs,buffer);
+      x = atof((char*)vs[0].c_str()) * BOHR_TO_ANGSTROM;
+      y = atof((char*)vs[1].c_str()) * BOHR_TO_ANGSTROM;
+      z = atof((char*)vs[2].c_str()) * BOHR_TO_ANGSTROM;
+      v3.Set(x,y,z);
+
+      OBUnitCell *uc = new OBUnitCell;
+      uc->SetData(v1,v2,v3);
+      mol.SetData(uc);
+
+      ifs.getline(buffer,BUFF_SIZE); // next line
+    }
+
   while (strstr(buffer,"$end") == NULL)
     {
       if (!ifs.getline(buffer,BUFF_SIZE)) break;
@@ -56,6 +85,24 @@ bool WriteDMol(ostream &ofs,OBMol &mol)
 {
   unsigned int i;
   char buffer[BUFF_SIZE];
+
+  if (mol.HasData(obUnitCell))
+    {
+      OBUnitCell *uc = (OBUnitCell*)mol.GetData(obUnitCell);
+      vector<vector3> v = uc->GetCellVectors();
+      vector3 v1;
+
+      ofs << "$cell vectors" << endl;
+      v1 = v[0] * ANGSTROM_TO_BOHR;
+      sprintf(buffer,"%-3s% 27.14f% 20.14f% 20.14f","", v1.x(), v1.y(), v1.z());
+      ofs << buffer << endl;
+      v1 = v[1] * ANGSTROM_TO_BOHR;
+      sprintf(buffer,"%-3s% 27.14f% 20.14f% 20.14f","", v1.x(), v1.y(), v1.z());
+      ofs << buffer << endl;
+      v1 = v[2] * ANGSTROM_TO_BOHR;
+      sprintf(buffer,"%-3s% 27.14f% 20.14f% 20.14f","", v1.x(), v1.y(), v1.z());
+      ofs << buffer << endl;
+    }
   
   ofs << "$coordinates" << endl;
 
