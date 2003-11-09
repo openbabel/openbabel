1a328654e9afd7fe68654316eadfe1644b15fb2d
Geoffrey Hutchison <geoff@geoffhutchison.net>
2003-11-09 04:54:52 +0000

    Improved support for ShelX, mostly due to contributions from Louis Richard

--- src/shelx.cpp
+++ src/shelx.cpp
@@ -31,23 +31,20 @@ bool ReadShelX(istream &ifs,OBMol &mol,const char *title)
   matrix3x3 m;
   
   ifs.getline(buffer,BUFF_SIZE); mol.SetTitle(buffer);
-  ifs.getline(buffer,BUFF_SIZE); sscanf(buffer,"%d",&natoms);
-  
   while (ifs.getline(buffer,BUFF_SIZE) &&!EQn(buffer,"CELL",4));
 
   if (!EQn(buffer,"CELL",4)) return(false);
   vector<string> vs;
   tokenize(vs,buffer," \n\t,");
-  if (vs.size() != 7) return(false);
+  if (vs.size() != 8) return(false);
 
   //parse cell values
-  A = atof((char*)vs[1].c_str());
-  B = atof((char*)vs[2].c_str());
-  C = atof((char*)vs[3].c_str());
-  Alpha = atof((char*)vs[4].c_str());
-  Beta  = atof((char*)vs[5].c_str());
-  Gamma = atof((char*)vs[6].c_str());
-
+  A = atof((char*)vs[2].c_str());
+  B = atof((char*)vs[3].c_str());
+  C = atof((char*)vs[4].c_str());
+  Alpha = atof((char*)vs[5].c_str());
+  Beta  = atof((char*)vs[6].c_str());
+  Gamma = atof((char*)vs[7].c_str());
   OBUnitCell *uc = new OBUnitCell;
   uc->SetData(A, B, C, Alpha, Beta, Gamma);
   mol.SetData(uc);
@@ -55,30 +52,38 @@ bool ReadShelX(istream &ifs,OBMol &mol,const char *title)
 
   int i;
   double x,y,z;
-  char type[10];
+  char type[10], *j;
   OBAtom *atom;
   vector3 v;
 
-  for (i = 1; i <= natoms;i++)
+  //skip until FVAR found
+  while (ifs.getline(buffer,BUFF_SIZE) &&!EQn(buffer,"FVAR",4));
+
+  //read atom records
+  while (ifs.getline(buffer,BUFF_SIZE) &&!EQn(buffer,"HKLF",4))
     {
-      if (!ifs.getline(buffer,BUFF_SIZE)) return(false);
       tokenize(vs,buffer," \n\t,");
-      if (vs.size() < 4) return(false);
+
+      //skip AFIX and PART instructions
+      if (vs.size() < 7) continue;
       atom = mol.NewAtom();
 
-      x = atof((char*)vs[1].c_str());
-      y = atof((char*)vs[2].c_str());
-      z = atof((char*)vs[3].c_str());
+      x = atof((char*)vs[2].c_str());
+      y = atof((char*)vs[3].c_str());
+      z = atof((char*)vs[4].c_str());
       v.Set(x,y,z); v *= m;
 
       strcpy(type,vs[0].c_str());
+      j = strpbrk(type, "0123456789"); j[0] = '\0';
       atom->SetAtomicNum(etab.GetAtomicNum(type));
       atom->SetVector(v);
-    }
+
+      //skip next line if anisotropic atoms.
+      if (vs.size() == 9) ifs.getline(buffer,BUFF_SIZE);
+    } //while
 
   mol.ConnectTheDots();
   mol.PerceiveBondOrders();
-
   return(true);
 }
 
