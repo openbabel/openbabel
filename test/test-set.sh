6e971b819080b8def773821443dbece69489699f
Geoffrey Hutchison <geoff@geoffhutchison.net>
2003-11-12 18:27:20 +0000

    Only mention running the test if the test         directory is there.

--- test/test-set.sh
+++ test/test-set.sh
@@ -2,11 +2,11 @@
 # test Open Babel with roundtripping if the test suite exists
 #   (just un-tar into this folder as "test-set" and it'll run)
 
-echo ${PACKAGE_VERSION}
-echo "Testing molecule file roundtripping..."
-echo "This test will take quite some time!"
-echo " and it currently always passes, so check the results file"
+echo
 if [ -d test-set ]; then
+    echo "Testing molecule file roundtripping..."
+    echo "This test will take quite some time!"
+    echo " and it currently always passes, so check the results file"
     cd test-set
     source test.sh
     cd ..
