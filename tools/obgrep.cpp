b53474438eeed678a27b1b02abcc9866fa4a8ec9
Fabien Fontaine <ffontaine@gmail.com>
2004-02-12 18:22:45 +0000

    -t option added to obgrep

--- tools/obgrep.cpp
+++ tools/obgrep.cpp
@@ -25,13 +25,25 @@ int main(int argc,char **argv)
 {
   char *program_name=NULL;
   io_type inFileType = UNDEFINED, outFileType = UNDEFINED;
-  int c; 
+  int c;
+  int ntimes=0;
+  bool pattern_matched=false, ntimes_matched=true;
   bool count=false, invert=false, full=false, name_only=false;
   char *FileIn = NULL, *Pattern = NULL;
-  
+  program_name = argv[0];
+
   // Parse options
-  while ((c = getopt(argc, argv, "nvcf")) != -1)
+  while ((c = getopt(argc, argv, "t:nvcf")) != -1)
     switch (c) {
+    case 't': // request ntimes unique matches
+      
+              c = sscanf(optarg, "%d", &ntimes);
+	      if (c != 1 ) {
+		cerr << program_name << ": unable to parse -t option" << endl;
+		exit (-1);
+	      }
+	      break;
+    
     case 'n': // print the molecule name only
               name_only = true;
 	      break;
@@ -56,7 +68,7 @@ int main(int argc,char **argv)
     }
 
   int index = optind;
-  program_name = argv[0];
+  
 
   if (argc-index != 2)
     {
@@ -64,11 +76,12 @@ int main(int argc,char **argv)
       err += program_name;
       err += " [options] \"PATTERN\" <filename>\n";
       err += "Options:\n";
-      err += "   -v    Invert the matching, print non-matching molecules\n";
-      err += "   -c    Print the number of matched molecules\n";
-      err += "   -f    Full match, print matching-molecules when the number\n";
-      err += "         of heavy atoms is equal to the number of PATTERN atoms\n";
-      err += "   -n    Only print the name of the molecules\n";
+      err += "   -v      Invert the matching, print non-matching molecules\n";
+      err += "   -c      Print the number of matched molecules\n";
+      err += "   -f      Full match, print matching-molecules when the number\n";
+      err += "           of heavy atoms is equal to the number of PATTERN atoms\n";
+      err += "   -n      Only print the name of the molecules\n";
+      err += "   -t NUM  Print a molecule only if the PATTERN occurs NUM times inside the molecule\n";
       ThrowError(err);
       exit(-1);
     }
@@ -95,7 +108,8 @@ int main(int argc,char **argv)
 
 
   // Match the SMART
-  OBSmartsPattern sp;
+  OBSmartsPattern sp; 
+  vector< vector <int> > maplist;      // list of matched atoms
   sp.Init(Pattern);
   ifstream ifs;
   
@@ -119,22 +133,56 @@ int main(int argc,char **argv)
       if (mol.Empty()) break;
       
       
-      // impossible to make a full match if the number of atoms is
+      ////////////////////////////////////////////////////////////////
+      // Do not loose time trying to match the pattern if the matching
+      // is impossible.
+      // It is impossible to make a full match if the number of atoms is
       // different
       if (full ) 
 	impossible_match = (sp.NumAtoms() == mol.NumHvyAtoms()) ? false : true;
       else
 	impossible_match = false;
 
-      if (impossible_match) { // avoid useless SMART matching attempt 
+      if (impossible_match) { // -> avoid useless SMART matching attempt 
 	if (invert) {
-	  if (!count) cout << mol;
+	  if (!count) {
+	    if ( name_only )
+	      cout << mol.GetTitle() << endl;
+	    else
+	      cout << mol;
+	  }
 	  c++;
 	}
 	continue;
       }
-	
-      if (sp.Match(mol) ) { // perform SMART matching
+
+
+      ////////////////////////////////////////////////////////////////
+      // perform SMART matching
+
+      pattern_matched = sp.Match(mol);
+      
+      // the number of times the match occured may matter
+      if ( ntimes ) { // ntimes is a positive integer of requested matches   
+	// Here, a match mean a unique match (same set of atoms)
+	// so we need to get the unique match list size
+
+	maplist = sp.GetUMapList();                
+
+	if( maplist.size() == ntimes )
+	  ntimes_matched = true;
+	else
+	  ntimes_matched = false;
+      } 
+      else  {  // ntimes == 0, we don't care about the number of matches
+	ntimes_matched = true; 
+      }
+
+
+      ////////////////////////////////////////////////////////////////
+      // perform a set of tests to guess what to print out
+
+      if ( pattern_matched == true && ntimes_matched == true) { 
 	if (!invert) {      // do something only when invert flag is off
 	  if (!count) {
 	    if ( name_only )
@@ -144,8 +192,10 @@ int main(int argc,char **argv)
 	  }
 	  c++;
 	}
+	
       }
-      else {                // compounds do not match the SMART
+    
+      else { // The SMART pattern do not occur as many times as requested
 	if (invert) {       // do something only if invert flag is on
 	  if (!count) {
 	    if ( name_only )
@@ -158,6 +208,9 @@ int main(int argc,char **argv)
       }
     } // end for loop
 
+
+  ////////////////////////////////////////////////////////////////
+  // Only print the number of matched molecules as requested
   if (count)
     cout << c << endl;
 
