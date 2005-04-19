a9ab702c6feee494a7c743f88902883fa109b95c
Geoffrey Hutchison <geoff@geoffhutchison.net>
2005-04-19 23:02:48 +0000

    * test/matrixtest.cpp: Include babelconfig.h.

--- test/matrixtest.cpp
+++ test/matrixtest.cpp
@@ -12,6 +12,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 ***********************************************************************/
 
+#include "babelconfig.h"
 #include "math/matrix3x3.h"
 #include "obutil.h"
 
