0ccf55656e28c6f999c47a75422b5552924fb20c
Chris Morley <c.morley@gaseq.co.uk>
2005-10-17 09:58:25 +0000

    Add GetType(); Trim title

--- src/formats/rxnformat.cpp
+++ src/formats/rxnformat.cpp
@@ -26,83 +26,89 @@ namespace OpenBabel
 class RXNFormat : public OBFormat
 {
 public:
-    //Register this format type ID
-    RXNFormat()
-    {
-        OBConversion::RegisterFormat("rxn",this);
-    }
+  //Register this format type ID
+  RXNFormat()
+  {
+      OBConversion::RegisterFormat("rxn",this);
+  }
 
-    virtual const char* Description()
-    {
-        return
-            "MDL RXN format\n \
-            \n";
-    };
+  virtual const char* Description()
+  {
+      return
+          "MDL RXN format\n \
+          \n";
+  };
 
   virtual const char* GetMIMEType() 
   { return "chemical/x-mdl-rxn"; };
 
-    virtual const char* TargetClassDescription()
-    {
-        return OBReaction::ClassDescription();
-    };
+  virtual const char* TargetClassDescription()
+  {
+      return OBReaction::ClassDescription();
+  };
 
-    ////////////////////////////////////////////////////
-    /// The "API" interface functions
-    virtual bool ReadMolecule(OBBase* pReact, OBConversion* pConv);
-    virtual bool WriteMolecule(OBBase* pReact, OBConversion* pConv);
+	const type_info& GetType()
+	{
+		return typeid(OBReaction*);
+	};
 
-    ////////////////////////////////////////////////////
-    /// The "Convert" interface functions
-    virtual bool ReadChemObject(OBConversion* pConv)
-    {
-        //Makes a new OBReaction and new associated OBMols
-        OBReaction* pReact = new OBReaction;
-        bool ret=ReadMolecule(pReact,pConv); //call the "API" read function
 
-	std::string auditMsg = "OpenBabel::Read reaction ";
-	std::string description(Description());
-	auditMsg += description.substr(0,description.find('\n'));
-	obErrorLog.ThrowError(__FUNCTION__,
-			      auditMsg,
-			      obAuditMsg);
+  ////////////////////////////////////////////////////
+  /// The "API" interface functions
+  virtual bool ReadMolecule(OBBase* pReact, OBConversion* pConv);
+  virtual bool WriteMolecule(OBBase* pReact, OBConversion* pConv);
 
-        if(ret) //Do transformation and return molecule
-					pConv->AddChemObject(pReact->DoTransformations(pConv->GetOptions(OBConversion::GENOPTIONS)));
-        else
-            pConv->AddChemObject(NULL);
-        return ret;
-    };
+  ////////////////////////////////////////////////////
+  /// The "Convert" interface functions
+  virtual bool ReadChemObject(OBConversion* pConv)
+  {
+    //Makes a new OBReaction and new associated OBMols
+    OBReaction* pReact = new OBReaction;
+    bool ret=ReadMolecule(pReact,pConv); //call the "API" read function
 
-    virtual bool WriteChemObject(OBConversion* pConv)
-    {
-        //WriteChemObject() always deletes the object retrieved by GetChemObject
-        //For RXN also deletes the associated molecules
-        //Cast to the class type need, e.g. OBMol
-        OBBase* pOb=pConv->GetChemObject();
-        OBReaction* pReact = dynamic_cast<OBReaction*>(pOb);
-        if(pReact==NULL)
-            return false;
+		std::string auditMsg = "OpenBabel::Read reaction ";
+		std::string description(Description());
+		auditMsg += description.substr(0,description.find('\n'));
+		obErrorLog.ThrowError(__FUNCTION__,
+							auditMsg,
+							obAuditMsg);
 
-        bool ret=false;
-        ret=WriteMolecule(pReact,pConv);
+		if(ret) //Do transformation and return molecule
+			pConv->AddChemObject(pReact->DoTransformations(pConv->GetOptions(OBConversion::GENOPTIONS)));
+		else
+				pConv->AddChemObject(NULL);
+		return ret;
+	};
 
-	std::string auditMsg = "OpenBabel::Write reaction ";
-	std::string description(Description());
-        auditMsg += description.substr( 0, description.find('\n') );
-        obErrorLog.ThrowError(__FUNCTION__,
-                              auditMsg,
-                              obAuditMsg);
+	virtual bool WriteChemObject(OBConversion* pConv)
+	{
+		//WriteChemObject() always deletes the object retrieved by GetChemObject
+		//For RXN also deletes the associated molecules
+		//Cast to the class type need, e.g. OBMol
+		OBBase* pOb=pConv->GetChemObject();
+		OBReaction* pReact = dynamic_cast<OBReaction*>(pOb);
+		if(pReact==NULL)
+				return false;
 
-        vector<OBMol*>::iterator itr;
-        for(itr=pReact->reactants.begin();itr!=pReact->reactants.end();itr++)
-            delete *itr;
-        for(itr=pReact->products.begin();itr!=pReact->products.end();itr++)
-            delete *itr;
+		bool ret=false;
+		ret=WriteMolecule(pReact,pConv);
 
-        delete pOb;
-        return ret;
-    };
+		std::string auditMsg = "OpenBabel::Write reaction ";
+		std::string description(Description());
+					auditMsg += description.substr( 0, description.find('\n') );
+					obErrorLog.ThrowError(__FUNCTION__,
+																auditMsg,
+																obAuditMsg);
+
+		vector<OBMol*>::iterator itr;
+		for(itr=pReact->reactants.begin();itr!=pReact->reactants.end();itr++)
+				delete *itr;
+		for(itr=pReact->products.begin();itr!=pReact->products.end();itr++)
+				delete *itr;
+
+		delete pOb;
+		return ret;
+	};
 };
 
 //Make an instance of the format class
@@ -131,6 +137,7 @@ bool RXNFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
     if (!ifs.getline(buffer,BUFF_SIZE))
         return(false); //reactiontitle
     pReact->title = buffer;
+		Trim(pReact->title);
     if (!ifs.getline(buffer,BUFF_SIZE))
         return(false); //creator
     if (!ifs.getline(buffer,BUFF_SIZE))
@@ -184,6 +191,7 @@ bool RXNFormat::WriteMolecule(OBBase* pOb, OBConversion* pConv)
         return false;
 
     OBConversion MolConv(*pConv); //new copy to use to write associated MOL
+		MolConv.SetAuxConv(NULL); //temporary until a proper OBConversion copy constructor written
 	   
 		OBFormat* pMolFormat = pConv->FindFormat("MOL");
     if(pMolFormat==NULL)
