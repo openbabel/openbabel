083da438d5769365720368547c670878387a395a
Geoffrey Hutchison <geoff@geoffhutchison.net>
2005-11-10 02:33:56 +0000

    * src/formats/balstformat.cpp: Add specification URL.

--- src/formats/balstformat.cpp
+++ src/formats/balstformat.cpp
@@ -42,7 +42,7 @@ public:
     };
 
   virtual const char* SpecificationURL()
-  {return "";}; //optional
+  {return "http://ocwww.chemie.uni-linz.ac.at/mueller/ball_and_stick.html";}; //optional
 
     //Flags() can return be any the following combined by | or be omitted if none apply
     // NOTREADABLE  READONEONLY  NOTWRITABLE  WRITEONEONLY
