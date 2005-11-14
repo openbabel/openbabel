697448c5a531b0081b92306fedda1d918ac33c36
Geoffrey Hutchison <geoff@geoffhutchison.net>
2005-11-14 16:03:11 +0000

    * scripts/perl/Makefile.PL, scripts/python/setup.py: Bump version         number for 1.0 release.

--- scripts/python/setup.py
+++ scripts/python/setup.py
@@ -12,7 +12,7 @@ obExtension = Extension('openbabel',
                         )
 
 setup(name='openbabel',
-      version='1.0.0b5',
+      version='1.0.0',
       description='Chemistry interface to Open Babel',
       author='Geoff Hutchison',
       author_email='openbabel-scripting@lists.sourceforge.net',
