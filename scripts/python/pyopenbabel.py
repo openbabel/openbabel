47ac0166f821dfa16db8c81d290a183f34d35911
Noel O'Boyle <baoilleach@gmail.com>
2005-12-09 09:22:46 +0000

    Added GetVar() methods to pyopenbabel.py.

--- scripts/python/pyopenbabel.py
+++ scripts/python/pyopenbabel.py
@@ -3,52 +3,72 @@ import openbabel as ob
 class Molecule(object):
     """Represent a molecule."""
 
-    _properties = {
-        'insertprops':'here'
+    _getmethods = {
+        'conformers':'GetConformers',
+        'coordinates':'GetCoordinates',
+        'data':'GetData',
+        'dim':'GetDimension',
+        'energy':'GetEnergy',
+        'exactmass':'GetExactMass',
+        'flags':'GetFlags',
+        'formula':'GetFormula',
+        'internalcoord':'GetInternalCoord',
+        'mod':'GetMod',
+        'molwt':'GetMolWt',
+        'sssr':'GetSSSR',
+        'title':'GetTitle',
+        'charge':'GetTotalCharge',
+        'spin':'GetTotalSpinMultiplicity'
     }
     
     def __init__(self):
         self.OBMol = ob.OBMol()
+        for x,v in self._getmethods.iteritems():
+            setattr( self,x,getattr(self,x) )
+        self.atoms = self.atoms
+    
     def __getattr__(self,attr):
         if attr == "atoms":
-            listofatoms = [ Atom(self.mol.GetAtom(i+1),i+1) for i in range(0,self.mol.NumAtoms()) ]
+            listofatoms = [ Atom(self.OBMol.GetAtom(i+1),i+1) for i in range(0,self.OBMol.NumAtoms()) ]
             return listofatoms
-    def newatom(self):
-        self.mol.NewAtom()
+        elif attr in self._getmethods:
+            return getattr(self.OBMol,self._getmethods[attr])()
+        else:
+            raise AttributeError,"Cannot find %s" % attr
 
 class Atom(object):
     """Represent an atom."""
     
-    _properties = {
-        'isotope':'GetIsotope',
-        'hyb':'GetHyb',
-        'cidx':'GetCIdx',
-        'valence':'GetValence',
-        'idx':'GetIdx',
-        'atomicnum':'GetAtomicNum',
-        'spin':'GetSpinMultiplicity',
-        'formalcharge':'GetFormalCharge',
-        'coordidx':'GetCoordinateIdx',
+    _getmethods = {
         'atomicmass':'GetAtomicMass',
-        'heterovalence':'GetHeteroValence',
+        'atomicnum':'GetAtomicNum',
+        'cidx':'GetCIdx',
+        'coordidx':'GetCoordinateIdx',
         'data':'GetData',
-        'heavyvalence':'GetHvyValence',
-        'vector':'GetVector',
-        'type':'GetType',
         'exactmass':'GetExactMass',
+        'formalcharge':'GetFormalCharge',
+        'heavyvalence':'GetHvyValence',
+        'heterovalence':'GetHeteroValence',
+        'hyb':'GetHyb',
+        'idx':'GetIdx',
         'implicitvalence':'GetImplicitValence',
+        'isotope':'GetIsotope',
         'partialcharge':'GetPartialCharge',
+        'spin':'GetSpinMultiplicity',
+        'type':'GetType',
+        'valence':'GetValence',
+        'vector':'GetVector',
         }
 
     def __init__(self,OBAtom=None,index=None):
-        # For the moment, I will remember the index of the atom in the molecule
-        # I'm not sure if this is useful.
+        # For the moment, I will remember the index of the atom in the molecule...
+        # I'm not sure if this is useful, though.
         if not OBAtom:
             OBAtom = ob.OBAtom()
         self.OBAtom = OBAtom
         self.index = index
         
-        for x,v in self._properties.iteritems():
+        for x,v in self._getmethods.iteritems():
             setattr( self,x,getattr(self,x) )
         self.coords = self.coords
     
@@ -56,8 +76,10 @@ class Atom(object):
         # Reminder to add corresponding __setattr__ methods
         if attr == "coords":
             return (self.OBAtom.GetX(),self.OBAtom.GetY(),self.OBAtom.GetZ())
-        elif attr in self._properties:
-            return getattr(self.OBAtom,self._properties[attr])()
+        elif attr in self._getmethods:
+            return getattr(self.OBAtom,self._getmethods[attr])()
+        else:
+            raise AttributeError,"Cannot find %s" % attr
 
     def __str__(self):
         line = []
