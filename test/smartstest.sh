f3950b9fa48ad6c3455b59d33e8ab9f5aefe3ce7
Geoffrey Hutchison <geoff@geoffhutchison.net>
2006-03-06 20:07:46 +0000

    * test/Makefile.am, test/Makefile.in, test/smartstest.sh,         test/ringtest.sh: New "wrapper" scripts to set BABEL_LIBDIR before         running tests.

---
+++ test/smartstest.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# This is just a small shell wrapper to be sure that we find the correct libs
+# since 'make check' is likely run before 'make install'
+#
+export BABEL_LIBDIR=../src/formats/.libs:../src/formats/xml/.libs
+
+./smartstest
