9ed6897b1c6a0ff8bd28e80ebbfb46c7ca7b42cc
Chris Morley <c.morley@gaseq.co.uk>
2006-03-09 11:52:47 +0000

    Add #include "babelconfig.h"

--- src/fingerprints/finger2.cpp
+++ src/fingerprints/finger2.cpp
@@ -16,6 +16,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 ***********************************************************************/
 
+#include "babelconfig.h"
 #include "mol.h"
 #include "fingerprint.h"
 #include <set>
