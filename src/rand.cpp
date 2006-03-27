48dcf15359112906b2aa97df571cf9e3ca036701
Michael Banck <mbanck@gmx.net>
2006-03-27 21:51:28 +0000

    2006-03-27  Michael Banck  <mbanck@gmx.net>
            * src/rand.cpp: Include <time.h> and <sys/time.h>.

--- src/rand.cpp
+++ src/rand.cpp
@@ -31,6 +31,17 @@ GNU General Public License for more details.
 #include "math/vector3.h"
 #include "obutil.h"
 
+#if TIME_WITH_SYS_TIME
+#include <sys/time.h>
+#include <time.h>
+#else
+#if HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif
+#endif
+
 #ifndef True
 #define True   1
 #define False  0
