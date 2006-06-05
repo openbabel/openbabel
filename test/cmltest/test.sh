e131628224bbedf127e78c75820075a749934517
Geoffrey Hutchison <geoff@geoffhutchison.net>
2006-06-05 17:37:24 +0000

    * test/cmltest/test.sh: Skip test if cml cannot be loaded.

--- test/cmltest/test.sh
+++ test/cmltest/test.sh
@@ -15,11 +15,20 @@ else
    export BABEL_LIBDIR
 fi
 
+# check to see if we have CML support or bail!
+cmltest=`${BABEL} -Hcml | grep "not recognized"`
+if [ "x${cmltest}" != "x" ]; then
+    echo "1..0 # skipping - CML format not loaded"
+    exit
+fi
+
+ROUNDTRIP=${builddir}/roundtrip
+
 # test input
 # CML2 with array
 echo "1..6"
 echo "# CML2 with array"
-if ($BABEL -icml cs2a.cml -omdl cs2a.mol && $builddir/roundtrip cs2a.cml cs2a.mol) then
+if (${BABEL} -icml cs2a.cml -omdl cs2a.mol && ${ROUNDTRIP} cs2a.cml cs2a.mol) then
     echo "ok 1"
 else
     echo "not ok 1"
@@ -28,7 +37,7 @@ fi
 #  3D molecules in SDF 
 #  CML2 with XML version
 echo "# CML2 with XML version"
-if ($BABEL -isdf 3d.head.sdf -ocml 3d.head.2.cml -x2v && $builddir/roundtrip 3d.head.sdf 3d.head.2.cml) then
+if (${BABEL} -isdf 3d.head.sdf -ocml 3d.head.2.cml -x2v && ${ROUNDTRIP} 3d.head.sdf 3d.head.2.cml) then
     echo "ok 2"
 else
     echo "not ok 2"
@@ -36,7 +45,7 @@ fi
 
 #  CML1 with DOCTYPE
 echo "# CML1 with DOCTYPE"
-if ($BABEL -isdf cs2a.mol -ocml cs2a.mol.cml -x1d && $builddir/roundtrip cs2a.mol cs2a.mol.cml); then
+if (${BABEL} -isdf cs2a.mol -ocml cs2a.mol.cml -x1d && ${ROUNDTRIP} cs2a.mol cs2a.mol.cml); then
     echo "ok 3"
 else
     echo "not ok 3"
@@ -45,7 +54,7 @@ fi
 #  CML2 arrays with namespaces (large)
 echo "# CML2 arrays with namespaces"
 # PR#1486678
-if ($BABEL -isdf 3d.head.sdf -ocml 3d.head.2an.cml -x2an && $builddir/roundtrip 3d.head.sdf 3d.head.2an.cml); then
+if (${BABEL} -isdf 3d.head.sdf -ocml 3d.head.2an.cml -x2an && ${ROUNDTRIP} 3d.head.sdf 3d.head.2an.cml); then
     echo "ok 4"
 else
     echo "not ok 4"
