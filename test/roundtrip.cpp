33031ce1f2b09631d13a17e7e13a008ba0476132
Geoffrey Hutchison <geoff@geoffhutchison.net>
2006-06-05 19:25:04 +0000

    * test/roundtrip.cpp: Fix faulty error problems with         multi-molecule CML files. Before trying a read at the end of the         file, make sure to clear molecular information.

--- test/roundtrip.cpp
+++ test/roundtrip.cpp
@@ -40,62 +40,62 @@ using namespace OpenBabel;
 
 int main(int argc,char *argv[])
 {
-    OBConversion conv;
-    OBFormat* pFormat1;
-    OBFormat* pFormat2;
+  OBConversion conv;
+  OBFormat* pFormat1;
+  OBFormat* pFormat2;
 
-    if (argc != 3)
+  if (argc != 3)
     {
-        cout << "Usage: roundtest <file1> <file2>" << endl;
-        return(-1);
+      cout << "Usage: roundtest <file1> <file2>" << endl;
+      return(-1);
     }
 
-    pFormat1 = conv.FormatFromExt(argv[1]);
-    if ( pFormat1 == NULL )
+  pFormat1 = conv.FormatFromExt(argv[1]);
+  if ( pFormat1 == NULL )
     {
-        cerr << argv[0] << ": Cannot read file #1 format!" << endl;
-        return(-1);
+      cerr << argv[0] << ": Cannot read file #1 format!" << endl;
+      return(-1);
     }
-    pFormat2 = conv.FormatFromExt(argv[2]);
-    if ( pFormat2 == NULL )
+  pFormat2 = conv.FormatFromExt(argv[2]);
+  if ( pFormat2 == NULL )
     {
-        cerr << argv[0] << ": Cannot read file #2 format!" << endl;
-        return(-1);
-    }
-
-    // Finally, we can do some work!
-    OBMol mol, mol2;
-    ifstream inFileStream1(argv[1]);
-    ifstream inFileStream2(argv[2]);
-
-    if (!inFileStream1)
-    {
-        cerr << argv[0] << ": Cannot read input file #1!" << endl;
-        return(-1);
-    }
-    else if (!inFileStream2)
-    {
-        cerr << argv[0] << ": Cannot read input file #2!" << endl;
-        return(-1);
+      cerr << argv[0] << ": Cannot read file #2 format!" << endl;
+      return(-1);
     }
 
-    OBAtom *atom1, *atom2;
-    OBConversion conv1(&inFileStream1, &cout), conv2(&inFileStream2, &cout);
+  // Finally, we can do some work!
+  OBMol mol, mol2;
+  ifstream inFileStream1(argv[1]);
+  ifstream inFileStream2(argv[2]);
 
-    if (! conv1.SetInAndOutFormats(pFormat1, pFormat2))
+  if (!inFileStream1)
     {
-        ThrowError("File format #1 isn't loaded");
-        return (-1);
+      cerr << argv[0] << ": Cannot read input file #1!" << endl;
+      return(-1);
     }
-    if (! conv2.SetInAndOutFormats(pFormat2, pFormat1))
+  else if (!inFileStream2)
+    {
+      cerr << argv[0] << ": Cannot read input file #2!" << endl;
+      return(-1);
+    }
+
+  OBAtom *atom1, *atom2;
+  OBConversion conv1(&inFileStream1, &cout), conv2(&inFileStream2, &cout);
+
+  if (! conv1.SetInAndOutFormats(pFormat1, pFormat2))
+    {
+      ThrowError("File format #1 isn't loaded");
+      return (-1);
+    }
+  if (! conv2.SetInAndOutFormats(pFormat2, pFormat1))
     {
       ThrowError("File format #2 isn't loaded");
       return (-1);
     }
 
-    int molCount = 0;
-    while(inFileStream1.peek() != EOF && inFileStream1.good() && 
-	  inFileStream2.peek() != EOF && inFileStream2.good() )
+  int molCount = 0;
+  while(inFileStream1.peek() != EOF && inFileStream1.good() && 
+        inFileStream2.peek() != EOF && inFileStream2.good() )
     {
       mol.Clear();
       mol2.Clear();
@@ -105,129 +105,131 @@ int main(int argc,char *argv[])
       conv1.Read(&mol);
       conv2.Read(&mol2);
 
-    const char* p1 = strrchr(argv[1],'.');
-    const char* p2 = strrchr(argv[2],'.');
+      const char* p1 = strrchr(argv[1],'.');
+      const char* p2 = strrchr(argv[2],'.');
 
-    if (p1 && strncasecmp(p1 + 1, "CML", 3) != 0
-	&& mol.NumAtoms() == 0)
-      {
-	cout << " ** ERROR ** molecule " << molCount 
-	     << " in file #1 has no atoms!" << endl;
-	return(-1);
-      }
+      if (p1 && strncasecmp(p1 + 1, "CML", 3) != 0
+          && mol.NumAtoms() == 0)
+        {
+          cout << " ** ERROR ** molecule " << molCount 
+               << " in file #1 has no atoms!" << endl;
+          return(-1);
+        }
 
-    if (p1 && strncasecmp(p1 + 1, "CML", 3) != 0
-	&& mol2.NumAtoms() == 0)
-      {
-	cout << " ** ERROR ** molecule " << molCount 
-	     << " in file #2 has no atoms!" << endl;
-	return(-1);
-      }
+      if (p1 && strncasecmp(p1 + 1, "CML", 3) != 0
+          && mol2.NumAtoms() == 0)
+        {
+          cout << " ** ERROR ** molecule " << molCount 
+               << " in file #2 has no atoms!" << endl;
+          return(-1);
+        }
 
-    if (p1 && strncasecmp(p1 + 1, "BOX", 3) == 0)
-      {
-	if (mol.NumAtoms() != 8)
-	  {
-	    cout << " *** ERROR *** BOX file #1 without 8 atoms!" << endl;
-	    return(-1);
-	  }
-	return(0);
-      }
+      if (p1 && strncasecmp(p1 + 1, "BOX", 3) == 0)
+        {
+          if (mol.NumAtoms() != 8)
+            {
+              cout << " *** ERROR *** BOX file #1 without 8 atoms!" << endl;
+              return(-1);
+            }
+          return(0);
+        }
 
-    if (p2 && strncasecmp(p2 + 1, "BOX", 3) == 0)
-      {
-	if (mol2.NumAtoms() != 8)
-	  {
-	    cout << " *** ERROR *** BOX file #2 without 8 atoms!" << endl;
-	    return(-1);
-	  }
-	return(0);
-      }
+      if (p2 && strncasecmp(p2 + 1, "BOX", 3) == 0)
+        {
+          if (mol2.NumAtoms() != 8)
+            {
+              cout << " *** ERROR *** BOX file #2 without 8 atoms!" << endl;
+              return(-1);
+            }
+          return(0);
+        }
 
-    if ( (p1 && strncasecmp(p1 + 1, "SMI", 3) == 0)
-	 || (p2 && strncasecmp(p2 + 1, "SMI", 3) == 0) )
-      {
-	if (mol.NumHvyAtoms() != mol2.NumHvyAtoms())
-	  {
-	    cout << " ** ERROR ** SMILES Number of heavy atoms differ: "
-		 << mol.NumHvyAtoms() << " and " << mol2.NumHvyAtoms() << endl;
-	    return(-1);
-	  }
-	return(0);
-      }
-    else
-      {
-	if (mol.NumAtoms() != mol2.NumAtoms())
-	  {
-	    cout << " ** ERROR ** Number of atoms differ: " << mol.NumAtoms()
-		 << " and " << mol2.NumAtoms()
-		 << " in molecule " << molCount << endl;
-	    return(-1);
-	  }
-      }
+      if ( (p1 && strncasecmp(p1 + 1, "SMI", 3) == 0)
+           || (p2 && strncasecmp(p2 + 1, "SMI", 3) == 0) )
+        {
+          if (mol.NumHvyAtoms() != mol2.NumHvyAtoms())
+            {
+              cout << " ** ERROR ** SMILES Number of heavy atoms differ: "
+                   << mol.NumHvyAtoms() << " and " << mol2.NumHvyAtoms() << endl;
+              return(-1);
+            }
+          return(0);
+        }
+      else
+        {
+          if (mol.NumAtoms() != mol2.NumAtoms())
+            {
+              cout << " ** ERROR ** Number of atoms differ: " << mol.NumAtoms()
+                   << " and " << mol2.NumAtoms()
+                   << " in molecule " << molCount << endl;
+              return(-1);
+            }
+        }
     
-    for(unsigned int i = 1;i <= mol.NumAtoms(); i++)
-      {
-	atom1 = mol.GetAtom(i);
-	atom2 = mol2.GetAtom(i);
+      for(unsigned int i = 1;i <= mol.NumAtoms(); i++)
+        {
+          atom1 = mol.GetAtom(i);
+          atom2 = mol2.GetAtom(i);
 	
-	if (atom1->GetAtomicNum() != atom2->GetAtomicNum())
-	  {
-	    cout << " ** ERROR ** Elements for atom " << i << " differ: " <<
-	      atom1->GetAtomicNum() << " and " << atom2->GetAtomicNum()
-		 << " in molecule " << molCount << endl;
-	    return(-1);
-	  }
+          if (atom1->GetAtomicNum() != atom2->GetAtomicNum())
+            {
+              cout << " ** ERROR ** Elements for atom " << i << " differ: " <<
+                atom1->GetAtomicNum() << " and " << atom2->GetAtomicNum()
+                   << " in molecule " << molCount << endl;
+              return(-1);
+            }
 	
-	if ( (p1 && strncasecmp(p1 + 1, "SMI", 3) == 0)
-	     || (p2 && strncasecmp(p2 + 1, "SMI", 3) == 0) )
-	  if ((atom1->GetX()-atom2->GetX()>1e-1) ||
-	      (atom1->GetY()-atom2->GetY()>1e-1) ||
-	      (atom1->GetZ()-atom2->GetZ()>1e-1))
-	    {
-	      cout << " ** ERROR ** Coordinates for atom " << i << " differ" 
-		   << " in molecule " << molCount << endl;
-	      return(-1);
-	    }
-      }
+          if ( (p1 && strncasecmp(p1 + 1, "SMI", 3) == 0)
+               || (p2 && strncasecmp(p2 + 1, "SMI", 3) == 0) )
+            if ((atom1->GetX()-atom2->GetX()>1e-1) ||
+                (atom1->GetY()-atom2->GetY()>1e-1) ||
+                (atom1->GetZ()-atom2->GetZ()>1e-1))
+              {
+                cout << " ** ERROR ** Coordinates for atom " << i << " differ" 
+                     << " in molecule " << molCount << endl;
+                return(-1);
+              }
+        }
 
     } // while reading molecules
 
-    char buffer[BUFF_SIZE];
-    // try skipping any blank lines
-    while(inFileStream1.peek() != EOF && inFileStream1.good() && 
-	  (inFileStream1.peek() == '\n' || inFileStream1.peek() == '\r'))
-      inFileStream1.getline(buffer,BUFF_SIZE);
+  char buffer[BUFF_SIZE];
+  // try skipping any blank lines
+  while(inFileStream1.peek() != EOF && inFileStream1.good() && 
+        (inFileStream1.peek() == '\n' || inFileStream1.peek() == '\r'))
+    inFileStream1.getline(buffer,BUFF_SIZE);
 
-    while(inFileStream2.peek() != EOF && inFileStream2.good() && 
-	  (inFileStream2.peek() == '\n' || inFileStream2.peek() == '\r'))
-      inFileStream2.getline(buffer,BUFF_SIZE);
+  while(inFileStream2.peek() != EOF && inFileStream2.good() && 
+        (inFileStream2.peek() == '\n' || inFileStream2.peek() == '\r'))
+    inFileStream2.getline(buffer,BUFF_SIZE);
 
-    // now check to see if there's anything to read
-    if ( inFileStream1.good() && inFileStream1.peek() != EOF &&
-	 conv1.Read(&mol) )
-      {
-	if (mol.NumAtoms() > 0)
-	  {
-	    cout << " ** ERROR **  File 1 has more molecules! " << endl;
-	    cout << "   Already read " << molCount << " molecules from both" << endl;
-	    cout << "   New molecule " << mol.GetTitle()
-		 << " has " << mol.NumAtoms() << " atoms " << endl;
-	    exit(-1);
-	  }
-      }
-    else if ( inFileStream2.good() && inFileStream2.peek() != EOF &&
-	      conv2.Read(&mol2) )
-      {
-	if (mol2.NumAtoms() > 0)
-	  {
-	    cout << " ** ERROR **  File 2 has more molecules! " << endl;
-	    cout << "   Already read " << molCount << " molecules from both" << endl;
-	    cout << "   New molecule " << mol.GetTitle() 
-		 << " has " << mol2.NumAtoms() << " atoms " << endl;
-	    exit(-1);
-	  }
-      }
+  // now check to see if there's anything to read
+  mol.Clear();
+  mol2.Clear();
+  if ( inFileStream1.good() && inFileStream1.peek() != EOF &&
+       conv1.Read(&mol) )
+    {
+      if (mol.NumAtoms() > 0)
+        {
+          cout << " ** ERROR **  File 1 has more molecules! " << endl;
+          cout << "   Already read " << molCount << " molecules from both" << endl;
+          cout << "   New molecule " << mol.GetTitle()
+               << " has " << mol.NumAtoms() << " atoms " << endl;
+          exit(-1);
+        }
+    }
+  else if ( inFileStream2.good() && inFileStream2.peek() != EOF &&
+            conv2.Read(&mol2) )
+    {
+      if (mol2.NumAtoms() > 0)
+        {
+          cout << " ** ERROR **  File 2 has more molecules! " << endl;
+          cout << "   Already read " << molCount << " molecules from both" << endl;
+          cout << "   New molecule " << mol.GetTitle() 
+               << " has " << mol2.NumAtoms() << " atoms " << endl;
+          exit(-1);
+        }
+    }
     
-    return(0);
+  return(0);
 }
