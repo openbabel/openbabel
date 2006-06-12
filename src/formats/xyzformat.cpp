255759630d3f8d244f953d54b31e03a74e801ca8
Geoffrey Hutchison <geoff@geoffhutchison.net>
2006-06-12 16:12:50 +0000

    * src/formats/xyzformat.cpp: Smarter handling of         "energy" tag in title -- if present, remove it from title. If         mol.GetEnergy() is near zero, don't print it.

--- src/formats/xyzformat.cpp
+++ src/formats/xyzformat.cpp
@@ -23,52 +23,53 @@ using namespace std;
 namespace OpenBabel
 {
 
-class XYZFormat : public OBMoleculeFormat
-{
-public:
-  //Register this format type ID
-  XYZFormat()
+  class XYZFormat : public OBMoleculeFormat
   {
-    OBConversion::RegisterFormat("xyz", this, "chemical/x-xyz");
-  }
+  public:
+    //Register this format type ID
+    XYZFormat()
+    {
+      OBConversion::RegisterFormat("xyz", this, "chemical/x-xyz");
+    }
 
-  virtual const char* Description() //required
-  {
-    return
-      "XYZ cartesian coordinates format\n \
+    virtual const char* Description() //required
+    {
+      return
+        "XYZ cartesian coordinates format\n \
        Read Options e.g. -as\n\
         s  Output single bonds only\n\
         b  Disable bonding entirely\n\n";
+    };
+
+    virtual const char* SpecificationURL()
+    {return "http://openbabel.sourceforge.net/formats/xyz.shtml";}; //optional
+
+    virtual const char* GetMIMEType() 
+    { return "chemical/x-xyz"; };
+
+    //*** This section identical for most OBMol conversions ***
+    ////////////////////////////////////////////////////
+    /// The "API" interface functions
+    virtual bool ReadMolecule(OBBase* pOb, OBConversion* pConv);
+    virtual bool WriteMolecule(OBBase* pOb, OBConversion* pConv);
   };
+  //***
 
-  virtual const char* SpecificationURL()
-  {return "http://openbabel.sourceforge.net/formats/xyz.shtml";}; //optional
+  //Make an instance of the format class
+  XYZFormat theXYZFormat;
 
-  virtual const char* GetMIMEType() 
-  { return "chemical/x-xyz"; };
-
-  //*** This section identical for most OBMol conversions ***
-  ////////////////////////////////////////////////////
-  /// The "API" interface functions
-  virtual bool ReadMolecule(OBBase* pOb, OBConversion* pConv);
-  virtual bool WriteMolecule(OBBase* pOb, OBConversion* pConv);
-};
-//***
-
-//Make an instance of the format class
-XYZFormat theXYZFormat;
-
-/////////////////////////////////////////////////////////////////
-bool XYZFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
-{
+  /////////////////////////////////////////////////////////////////
+  bool XYZFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
+  {
     OBMol* pmol = dynamic_cast<OBMol*>(pOb);
     if(pmol==NULL)
-        return false;
+      return false;
 
     //Define some references so we can use the old parameter names
     istream &ifs = *pConv->GetInStream();
     OBMol &mol = *pmol;
     const char* title = pConv->GetTitle();
+
     char buffer[BUFF_SIZE];
 
     stringstream errorMsg;
@@ -77,17 +78,17 @@ bool XYZFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
 
     if (!ifs.getline(buffer,BUFF_SIZE))
       {
-	obErrorLog.ThrowError(__FUNCTION__,
-			      "Problems reading an XYZ file: Cannot read the first line.", obWarning);
-	return(false);
+        obErrorLog.ThrowError(__FUNCTION__,
+                              "Problems reading an XYZ file: Cannot read the first line.", obWarning);
+        return(false);
       }
 
     if (sscanf(buffer, "%d", &natoms) == 0 || !natoms)
-    {
-      obErrorLog.ThrowError(__FUNCTION__,
-			    "Problems reading an XYZ file: The first line must contain the number of atoms.", obWarning);
-      return(false);
-    }
+      {
+        obErrorLog.ThrowError(__FUNCTION__,
+                              "Problems reading an XYZ file: The first line must contain the number of atoms.", obWarning);
+        return(false);
+      }
 
     mol.ReserveAtoms(natoms);
 
@@ -95,15 +96,22 @@ bool XYZFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
     // as the title for the molecule if the line is not
     // empty. Otherwise, use the title given by the calling function.
     if (!ifs.getline(buffer,BUFF_SIZE))
-    {
-      obErrorLog.ThrowError(__FUNCTION__,
-			    "Problems reading an XYZ file: Could not read the second line (title/comments).", obWarning);
+      {
+        obErrorLog.ThrowError(__FUNCTION__,
+                              "Problems reading an XYZ file: Could not read the second line (title/comments).", obWarning);
         return(false);
-    }
-    if (strlen(buffer) != 0)
-        mol.SetTitle(buffer);
+      }
+    string readTitle(buffer);
+    string::size_type location = readTitle.find("Energy");
+    if (location != string::npos)
+      readTitle.erase(location);
+    Trim(readTitle);
+
+    location = readTitle.find_first_not_of(" \t\n\r");
+    if (location != string::npos)
+      mol.SetTitle(readTitle);
     else
-        mol.SetTitle(title);
+      mol.SetTitle(title);
 
     mol.BeginModify();
 
@@ -111,29 +119,29 @@ bool XYZFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
     // spaces: the atom type, and the coordinates of the atom
     vector<string> vs;
     for (unsigned int i = 1; i <= natoms; i ++)
-    {
+      {
         if (!ifs.getline(buffer,BUFF_SIZE))
-        {
-	  errorMsg << "Problems reading an XYZ file: "
-		   << "Could not read line #" << i+2 << ", file error." << endl
-		   << " According to line one, there should be " << natoms 
-		   << " atoms, and therefore " << natoms+2 << " lines in the file.";
+          {
+            errorMsg << "Problems reading an XYZ file: "
+                     << "Could not read line #" << i+2 << ", file error." << endl
+                     << " According to line one, there should be " << natoms 
+                     << " atoms, and therefore " << natoms+2 << " lines in the file.";
 
-	  obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
-	  return(false);
-        }
+            obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
+            return(false);
+          }
         tokenize(vs,buffer);
         if (vs.size() != 4)
-        {
-          errorMsg << "Problems reading an XYZ file: "
-		   << "Could not read line #" << i+2 << "." << endl
-		   << "OpenBabel found the line '" << buffer << "'" << endl
-		   << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
-		   << "However, OpenBabel found " << vs.size() << " items.";
+          {
+            errorMsg << "Problems reading an XYZ file: "
+                     << "Could not read line #" << i+2 << "." << endl
+                     << "OpenBabel found the line '" << buffer << "'" << endl
+                     << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
+                     << "However, OpenBabel found " << vs.size() << " items.";
 
-	  obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
-	  return(false);
-        }
+            obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
+            return(false);
+          }
 
         // Atom Type: get the atomic number from the element table, using
         // the first entry in the currently read line. If the entry makes
@@ -146,52 +154,52 @@ bool XYZFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
         int atomicNum = etab.GetAtomicNum(vs[0].c_str());
         atom->SetAtomicNum(atomicNum); //set atomic number, or '0' if the atom type is not recognized
         if (atomicNum == 0)
-            atom->SetType(vs[0]);
+          atom->SetType(vs[0]);
 
         // Read the atom coordinates
         char *endptr;
         double x = strtod((char*)vs[1].c_str(),&endptr);
         if (endptr == (char*)vs[1].c_str())
-        {
-          errorMsg << "Problems reading an XYZ file: "
-		   << "Could not read line #" << i+2 << "." << endl
-		   << "OpenBabel found the line '" << buffer << "'" << endl
-		   << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
-		   << "OpenBabel could not interpret item #1 as a number.";
+          {
+            errorMsg << "Problems reading an XYZ file: "
+                     << "Could not read line #" << i+2 << "." << endl
+                     << "OpenBabel found the line '" << buffer << "'" << endl
+                     << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
+                     << "OpenBabel could not interpret item #1 as a number.";
 
-	  obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
-	  return(false);
-        }
+            obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
+            return(false);
+          }
         double y = strtod((char*)vs[2].c_str(),&endptr);
         if (endptr == (char*)vs[2].c_str())
-        {
-          errorMsg << "Problems reading an XYZ file: "
-		   << "Could not read line #" << i+2 << "." << endl
-		   << "OpenBabel found the line '" << buffer << "'" << endl
-		   << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
-		   << "OpenBabel could not interpret item #2 as a number.";
+          {
+            errorMsg << "Problems reading an XYZ file: "
+                     << "Could not read line #" << i+2 << "." << endl
+                     << "OpenBabel found the line '" << buffer << "'" << endl
+                     << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
+                     << "OpenBabel could not interpret item #2 as a number.";
 
-	  obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
-	  return(false);
-        }
+            obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
+            return(false);
+          }
         double z = strtod((char*)vs[3].c_str(),&endptr);
         if (endptr == (char*)vs[3].c_str())
-        {
-          errorMsg << "Problems reading an XYZ file: "
-		   << "Could not read line #" << i+2 << "." << endl
-		   << "OpenBabel found the line '" << buffer << "'" << endl
-		   << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
-		   << "OpenBabel could not interpret item #3 as a number.";
+          {
+            errorMsg << "Problems reading an XYZ file: "
+                     << "Could not read line #" << i+2 << "." << endl
+                     << "OpenBabel found the line '" << buffer << "'" << endl
+                     << "According to the specifications, this line should contain exactly 4 entries, separated by white space." << endl
+                     << "OpenBabel could not interpret item #3 as a number.";
 
-	  obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
-	  return(false);
-        }
+            obErrorLog.ThrowError(__FUNCTION__, errorMsg.str() , obWarning);
+            return(false);
+          }
         atom->SetVector(x,y,z); //set coordinates
-    }
+      }
 
     // clean out any remaining blank lines
     while(ifs.peek() != EOF && ifs.good() && 
-	  (ifs.peek() == '\n' || ifs.peek() == '\r'))
+          (ifs.peek() == '\n' || ifs.peek() == '\r'))
       ifs.getline(buffer,BUFF_SIZE);
 
     if (!pConv->IsOption("b",OBConversion::INOPTIONS))
@@ -202,15 +210,15 @@ bool XYZFormat::ReadMolecule(OBBase* pOb, OBConversion* pConv)
     mol.EndModify();
 
     return(true);
-}
+  }
 
-////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////
 
-bool XYZFormat::WriteMolecule(OBBase* pOb, OBConversion* pConv)
-{
+  bool XYZFormat::WriteMolecule(OBBase* pOb, OBConversion* pConv)
+  {
     OBMol* pmol = dynamic_cast<OBMol*>(pOb);
     if(pmol==NULL)
-        return false;
+      return false;
 
     //Define some references so we can use the old parameter names
     ostream &ofs = *pConv->GetOutStream();
@@ -221,23 +229,27 @@ bool XYZFormat::WriteMolecule(OBBase* pOb, OBConversion* pConv)
 
     snprintf(buffer, BUFF_SIZE, "%d", mol.NumAtoms());
     ofs << buffer << endl;
-    snprintf(buffer, BUFF_SIZE, "%s", mol.GetTitle());
+    if (fabs(mol.GetEnergy()) > 1.0e-3) // nonzero energy field
+      snprintf(buffer, BUFF_SIZE, "%s\tEnergy: %15.7f",
+               mol.GetTitle(), mol.GetEnergy());
+    else
+      snprintf(buffer, BUFF_SIZE, "%s", mol.GetTitle());
     ofs << buffer << endl;
 
     OBAtom *atom;
     string str,str1;
     for(i = 1;i <= mol.NumAtoms(); i++)
-    {
+      {
         atom = mol.GetAtom(i);
         snprintf(buffer, BUFF_SIZE, "%3s%15.5f%15.5f%15.5f",
-                etab.GetSymbol(atom->GetAtomicNum()),
-                atom->GetX(),
-                atom->GetY(),
-                atom->GetZ());
+                 etab.GetSymbol(atom->GetAtomicNum()),
+                 atom->GetX(),
+                 atom->GetY(),
+                 atom->GetZ());
         ofs << buffer << endl;
-    }
+      }
 
     return(true);
-}
+  }
 
 } //namespace OpenBabel
