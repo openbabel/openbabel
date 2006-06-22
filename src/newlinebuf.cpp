84ba9e83811a3a3c6f8efb05cdb41f2f7344561f
Geoffrey Hutchison <geoff@geoffhutchison.net>
2006-06-22 22:47:10 +0000

    * src/newlinebuf.h, src/newlinebuf.cpp: New "filtering streambuf"         class to filter line-endings, e.g. \r\n or \r -> \n expected by         much of the code. Fixes PR#1510008 and goes towards feature         request PR#1499615. Currently unused until testing is complete.
            * src/Makefile.am, src/Makefile.in: Add newlinebuf.

---
+++ src/newlinebuf.cpp
@@ -0,0 +1,73 @@
+/**********************************************************************
+newlinebuf.cpp - Filter line endings, converting \r or \r\n -> \n
+ 
+Copyright (C) 2005-2006 by Geoffrey R. Hutchison
+ 
+This file is part of the Open Babel project.
+For more information, see <http://openbabel.sourceforge.net/>
+ 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+ 
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+***********************************************************************/
+
+#include "babelconfig.h"
+#include "newlinebuf.h"
+
+#include <streambuf>
+
+namespace OpenBabel
+{
+  newlinebuf::newlinebuf(std::streambuf *sb) :
+    _internalBuf(sb), _returnChar(false)
+  {
+    setg(_buffer, _buffer, _buffer);
+  }
+
+  newlinebuf::~newlinebuf()
+  {
+    delete _buffer;
+  }
+    
+  int newlinebuf::underflow()
+  {
+    char *ptr = _buffer;
+    int ch;
+    while ( (ptr != _buffer + sizeof(_buffer) - 2) 
+            && ((ch = _internalBuf->sgetc()) != EOF) )
+      {
+        _internalBuf->sbumpc();
+
+        switch (ch)
+          {
+          case 13:
+            _returnChar = true;
+            break;
+          case 10:
+            _returnChar = false;
+          default:
+            if (_returnChar) // just \r line ending
+              {
+                _returnChar = false;
+                *ptr++ = '\n';
+              }
+            *ptr++ = ch;
+          }
+      }
+
+    if (ptr == _buffer)
+      return EOF;
+
+    setg(_buffer, _buffer, ptr);
+    return *_buffer;
+  }
+
+} // end namespace OpenBabel
+
+//! \file newlinebuf.cpp
+//! \brief Filter line endings -> '\n'
