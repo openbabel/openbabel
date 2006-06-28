f938ed53157fe5bb5bda3da5df98863954ef9df9
Geoffrey Hutchison <geoff@geoffhutchison.net>
2006-06-28 21:06:14 +0000

    * configure, configure.in, src/all.h, src/formats/all.h,         Makefile.in: Add experimental support for precompiled headers with         GCC, using the --enable-precompiled-headers flag to the configure         script. Haven't performed much timing/optimization -- appears to         help about 10-12%.

---
+++ src/formats/all.h
@@ -0,0 +1,43 @@
+/**********************************************************************
+all.h - Target for Precompiled Header Support (includes most common headers)
+ 
+Copyright (C) 2005-2006 by Geoffrey R. Hutchison
+ 
+This file is part of the Open Babel project.
+For more information, see <http://openbabel.sourceforge.net/>
+ 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+***********************************************************************/
+
+#ifndef OB_ALL_H
+#define OB_ALL_H
+
+#include "babelconfig.h"
+
+// Common STL headers
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
+#include "base.h"
+#include "generic.h"
+#include "mol.h"
+#include "oberror.h"
+#include "obconversion.h"
+#include "obmolecformat.h"
+
+#endif OB_ALL_H
+
+//! \file all.h
+//! \brief Target header for precompiled header support
