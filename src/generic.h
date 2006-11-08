d41ad5099d7d8dd4be47a619e64352e5a9700f7a
Donald Ephraim Curtis <dcurtis3@sourceforge.net>
2006-11-08 19:33:27 +0000

    Added GetData(const char *s) to OBSetData so that you can retrieve data from a const char rather than having to instanciate a string.

--- src/generic.h
+++ src/generic.h
@@ -296,6 +296,18 @@ namespace OpenBabel
     }
 
     //! Returns the OBGenericData associate with the attribute name parameter.
+    OBGenericData *GetData(const char *s)
+    {
+      std::vector<OBGenericData*>::iterator i;
+
+      for (i = _vdata.begin();i != _vdata.end();i++)
+        if ((*i)->GetAttribute() == s)
+          return(*i);
+
+      return(NULL);
+    }
+
+    //! Returns the OBGenericData associate with the attribute name parameter.
     OBGenericData *GetData(const std::string &s)
     {
       std::vector<OBGenericData*>::iterator i;
