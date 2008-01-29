4fb392dc417673dceb28c7842bbe26bf0e40727a
Joshua Ballanco <Joshua.Ballanco@stevens.edu>
2008-01-29 23:28:05 +0000

    2008-01-29  Joshua Ballanco <jballanc@gmail.com>
    	* scripts/ruby/extconf.rb: Fixed an error that had Ruby 1.8.6
    	using gcc to compile and link the C++ extension file. Also updated
    	the method of finding the correct include directories.

--- scripts/ruby/extconf.rb
+++ scripts/ruby/extconf.rb
@@ -1,18 +1,40 @@
 # Creates a makefile to build the Open Babel-Ruby extension.
 
-# This tells mkmf where to look for the Open Babel headers. There's probably a better way to do it, but the mkmf documentation doesn't say how.
-if (ARGV.size == 0)
-  ARGV << "--with-openbabel-include=../../include"
+# Compensate for the fact that OpenBabel on OS X will not compile
+# as a universal binary by default.
+if system('uname > /dev/null') && `uname -smr` =~ /^Darwin 9.* i386$/
+  ENV['ARCHFLAGS'] = '-arch i386'
 end
 
 require 'mkmf'
 
 dir_config('openbabel')
 
+# Find a trivial header in order to add the proper include path
+# to the build flags.
+find_header('inchi_api.h', '../../include')
+
+# Prevent Ruby 1.8.x from trying to compile and link the extension
+# using gcc.
+if RUBY_VERSION < "1.9"
+  cxx = ''
+  begin
+    File.open('../Makefile', 'r').each_line do |line|
+      if line =~ /CXX = /
+        cxx = Regexp.last_match.post_match.chomp
+      end
+    end
+  rescue Errno::ENOENT
+    puts 'Please configure OpenBabel before compiling the Ruby extension'
+  end
+  
+  cpp_command(cxx)
+  CONFIG['LDSHARED'] = "#{cxx} #{ENV['ARCHFLAGS']} -pipe -bundle"
+end
+
 if have_library('openbabel')
   create_makefile('openbabel')
 else
   puts "Install Open Babel first. If you've already compiled and installed Open Babel, you may need to run ldconfig."
 end
-
 
