799a9fad12984f7520ba504b3a051c4f4ed76dcd
Geoffrey Hutchison <geoff@geoffhutchison.net>
2008-03-21 19:00:07 +0000

    * tools/obfragment.cpp: Update to generate new OBBuilder database.

--- tools/obfragment.cpp
+++ tools/obfragment.cpp
@@ -36,6 +36,8 @@ extern "C" int strncasecmp(const char *s1, const char *s2, size_t n);
 using namespace std;
 using namespace OpenBabel;
 
+string RewriteSMILES(const string smiles);
+
 int main(int argc,char *argv[])
 {
   // turn off slow sync with C-style output (we don't use it anyway).
@@ -86,6 +88,7 @@ int main(int argc,char *argv[])
     while(ifs.peek() != EOF && ifs.good())
       {
         conv.Read(&mol, &ifs);
+        if (!mol.Has3D()) continue; // invalid coordinates!
         mol.DeleteHydrogens(); // remove these before we do anything else
         
         do {
@@ -98,10 +101,10 @@ int main(int argc,char *argv[])
                 mol.DeleteAtom(atom);
                 nonRingAtoms = true;
                 break; // don't know how many atoms there are
-              } else {
-                atom->SetAtomicNum(6);
-                atom->SetFormalCharge(0);
-              }
+              } 
+              // Previously, we changed atoms to carbon here.
+              // Now we perform this alchemy in terms of string-rewriting
+              // once the canonical SMILES is generated
             }
         } while (nonRingAtoms);
         
@@ -130,8 +133,9 @@ int main(int argc,char *argv[])
           {
             if (fragments[i].NumAtoms() < 3) // too small to care
               continue;
-
+              
             currentCAN = conv.WriteString(&fragments[i], true);
+            currentCAN = RewriteSMILES(currentCAN); // change elements to "a/A" for compression
             if (index.find(currentCAN) != index.end()) { // already got this
               index[currentCAN] += 1; // add to the count for bookkeeping
               continue;
@@ -141,25 +145,28 @@ int main(int argc,char *argv[])
 
             // OK, now retrieve the canonical ordering for the fragment
             vector<string> canonical_order;
-            if (fragments[i].HasData("canonical order")) {
-              OBPairData *data = (OBPairData*)fragments[i].GetData("canonical order");
+            if (fragments[i].HasData("Canonical Atom Order")) {
+              OBPairData *data = (OBPairData*)fragments[i].GetData("Canonical Atom Order");
               tokenize(canonical_order, data->GetValue().c_str());
             }
 
             // Write out an XYZ-style file with the CANSMI as the title
             cout << fragments[i].NumAtoms() << '\n';
-            cout << "INDEX " << currentCAN << '\n'; // endl causes a flush
+            cout << currentCAN << '\n'; // endl causes a flush
 
             vector<string>::iterator can_iter;
             unsigned int order;
             OBAtom *atom;
+
+            fragments[i].Center();
+            fragments[i].ToInertialFrame();
 
             for (unsigned int index = 0; index < canonical_order.size(); 
                  ++index) {
               order = atoi(canonical_order[index].c_str());
               atom = fragments[i].GetAtom(order);
               
-              snprintf(buffer, BUFF_SIZE, "C %9.3f %9.3f %9.3f\n",
+              snprintf(buffer, BUFF_SIZE, "C%8.3f%8.3f%8.3f\n",
                        atom->x(), atom->y(), atom->z());
               cout << buffer;
             }
@@ -183,4 +190,66 @@ int main(int argc,char *argv[])
   }
     
   return(0);
+}
+
+// Replace all instances of a string pattern with another
+// (This should be part of the STL, but isn't, sadly.)
+void FindAndReplace( string &source, const string find, const string replace )
+ {
+	 size_t j;
+	 for ( ; (j = source.find( find )) != string::npos ; ) {
+		 source.replace( j, find.length(), replace );
+	 }
+ }
+
+string RewriteSMILES(const string smiles)
+{
+  string fragment = smiles;
+  
+  // This is a bit tedious -- we must place all atom types here
+  // It would be much easier with a regex
+
+  // NOTE: You must replace the two-letter elements first
+  // (since the one-letter elements would match too, e.g. C replacing Cl)
+  FindAndReplace(fragment, "[Cl]", "A");
+  FindAndReplace(fragment, "[Se]", "A");
+  FindAndReplace(fragment, "[Br]", "A");
+  FindAndReplace(fragment, "[Al]", "A");
+  FindAndReplace(fragment, "[Si]", "A");
+  FindAndReplace(fragment, "[As]", "A");
+  FindAndReplace(fragment, "[Li]", "A");
+  FindAndReplace(fragment, "[Na]", "A");
+  FindAndReplace(fragment, "[Mg]", "A");
+  FindAndReplace(fragment, "[Ca]", "A");
+  FindAndReplace(fragment, "[Cr]", "A");
+  FindAndReplace(fragment, "[Mn]", "A");
+  FindAndReplace(fragment, "[Fe]", "A");
+  FindAndReplace(fragment, "[Co]", "A");
+  FindAndReplace(fragment, "[Ni]", "A");
+  FindAndReplace(fragment, "[Zn]", "A");
+  FindAndReplace(fragment, "[Cu]", "A");
+  FindAndReplace(fragment, "B", "A");
+  FindAndReplace(fragment, "C", "A");
+  FindAndReplace(fragment, "N", "A");
+  FindAndReplace(fragment, "O", "A");
+  FindAndReplace(fragment, "F", "A");
+  FindAndReplace(fragment, "P", "A");
+  FindAndReplace(fragment, "S", "A");
+  FindAndReplace(fragment, "I", "A");
+  FindAndReplace(fragment, "K", "A");
+  
+  // And now some aromatics
+  FindAndReplace(fragment, "[si]", "a");
+  FindAndReplace(fragment, "[se]", "a");
+  FindAndReplace(fragment, "b", "a");
+  FindAndReplace(fragment, "c", "a");
+  FindAndReplace(fragment, "n", "a");
+  FindAndReplace(fragment, "o", "a");
+  FindAndReplace(fragment, "p", "a");
+  FindAndReplace(fragment, "s", "a");
+  
+  // There are probably other elements which might be ignored, but these are rare
+  // (i.e., unlikely to be top ring fragment hits)
+  
+  return fragment;
 }
