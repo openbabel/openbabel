a59bae49c3fca239448bddd92f65196bc6725583
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2008-09-11 22:07:15 +0000

    Forgot obfunction.h

--- include/openbabel/obfunction.h
+++ include/openbabel/obfunction.h
@@ -58,6 +58,37 @@ namespace OpenBabel {
        * @brief Get the atom gradients.
        */
       std::vector<Eigen::Vector3d>&  GetGradients() { return m_gradients; } 
+      /** 
+       * @brief Calculate the potential energy function derivative numerically with 
+       * repect to the coordinates of atom with index @p idx (this vector is the gradient)
+       *
+       * @code
+       *         f(1) - f(0)
+       * f'(0) = -----------      f(1) = f(0+h)
+       *              h
+       * @endcode
+       *
+       * @param idx Index for the atom starting at 0.
+       * 
+       * @return The gradient for atom with index @p idx.
+       */
+      Eigen::Vector3d NumericalDerivative(unsigned int idx);
+      /** 
+       * @brief Calculate the potential energy function 2nd derivative numerically with 
+       * repect to the coordinates of atom with index @p idx.
+       *
+       * @code
+       *         f(2) - 2f(1) + f(0)
+       * f'(0) = -------------------      f(1) = f(0+h)
+       *                 h^2              f(1) = f(0+2h)
+       * @endcode
+       *
+       * @param idx Index for the atom starting at 0.
+       * 
+       * @return The 2nd derivative for atom with index @p idx.
+       */ 
+      Eigen::Vector3d NumericalSecondDerivative(unsigned int idx);
+ 
   };
 
 } // namespace OpenBabel
