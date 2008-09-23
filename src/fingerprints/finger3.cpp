6e95227fbedfa98eb7c98bdd39ed202e5666d192
Chris Morley <c.morley@gaseq.co.uk>
2008-09-23 20:37:56 +0000

    Enhanced PatternFP and MACCS fingerprint

--- src/fingerprints/finger3.cpp
+++ src/fingerprints/finger3.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-finger3.cpp: Fingerprint based on list of SMARTS patterns
+finger3.cpp: Fingerprints based on list of SMARTS patterns
 Copyright (C) 2005 Chris Morley
  
 This file is part of the Open Babel project.
@@ -18,11 +18,7 @@ GNU General Public License for more details.
 #include <openbabel/babelconfig.h>
 #include <openbabel/mol.h>
 
-#ifdef HAVE_SSTREAM
 #include <sstream>
-#else
-#include <strstream>
-#endif
 #include <fstream>
 #include <map>
 #include <string>
@@ -32,11 +28,22 @@ GNU General Public License for more details.
 using namespace std;
 namespace OpenBabel
 {
-/// \brief Fingerprint based on list of SMARTS patterns ID="FP3"
+/// \brief Fingerprint based on list of SMARTS patterns
 class PatternFP  : public OBFingerprint
 {
 private:
-  vector<string> smartsStrings;
+  struct pattern
+  {
+    string smartsstring;
+    OBSmartsPattern obsmarts;
+    string description;
+    int numbits;
+    int numoccurrences;
+    int bitindex;
+  };
+  vector<pattern> _pats;
+  int _bitcount;
+
 protected:
   string _patternsfile;
 
@@ -48,158 +55,261 @@ public:
       _patternsfile="patterns.txt";
     else
       _patternsfile = filename;
-  };
-  
+  }
+
+///////////////////////////////////////////////////////////////////////////// 
   virtual const char* Description()
   {
     static string desc;
-    desc = "SMARTS patterns specified in the file " + _patternsfile;
+    desc = "SMARTS patterns specified in the file " + _patternsfile
+      + "\nPatternFP is definable";
     return (desc.c_str());
-  };
+  }
 
+//////////////////////////////////////////////////////////////////////////////
   //Each bit represents a single substructure; no need for confirmation when substructure searching
   virtual unsigned int Flags() { return FPT_UNIQUEBITS;}; 
 
-  bool GetFingerprint(OBBase* pOb, vector<unsigned int>&fp, int nbits) 
+///////////////////////////////////////////////////////////////////////////////
+  virtual PatternFP* MakeInstance(const std::vector<std::string>& textlines)
+  {
+    return new PatternFP(textlines[1].c_str(),textlines[2].c_str());
+  }
+
+//////////////////////////////////////////////////////////////////////////////// 
+  virtual bool GetFingerprint(OBBase* pOb, vector<unsigned int>&fp, int foldbits) 
   {
     OBMol* pmol = dynamic_cast<OBMol*>(pOb);
     if(!pmol)
       return false;
     
+    unsigned int n;
     //Read patterns file if it has not been done already
-    if(smartsStrings.empty())
-      ReadPatternFile(_patternsfile, smartsStrings);
+    if(_pats.empty())
+      ReadPatternFile();
 
     //Make fp size the smallest power of two to contain the patterns
-    unsigned int n=Getbitsperint();
-    while(n<smartsStrings.size())n*=2;
+    n=Getbitsperint();
+    while(n < _bitcount)
+      n*=2;
     fp.resize(n/Getbitsperint());
 
-    for(n=0;n<smartsStrings.size();++n)
+    n=0; //bit position
+    vector<pattern>::iterator ppat;
+    for(ppat=_pats.begin();ppat!=_pats.end();++ppat)
     {
-      OBSmartsPattern sp;
-      sp.Init(smartsStrings[n]);
-      if(sp.Match(*pmol, true))
-        SetBit(fp, n);
+      if(ppat->numbits //ignore pattern if numbits==0
+        && ppat->obsmarts.Match(*pmol, ppat->numoccurrences==0))//do single match if all that's needed
+      {
+        /* Set bits in the fingerprint depending on the number of matches in the molecule
+           and the parameters, numbits and numoccurrences, in the pattern.
+           The pattern will set or clear numbits bits in the fingerprint.
+           They will be in numoccurrences+1 groups, each containing an approximately
+           equal number of bits.
+           The first group of bits will be set if numMatches > numoccurences;
+           The second group will be set if numMatches > numoccurrences - 1;
+           and so on.
+           So with a pattern with numbits = 4 and numoccurences = 2,
+           the groups would be 1, 1, and 2 bits.
+           A molecule with
+              1 match to the pattern would give 0011 
+              2 matches to the pattern would give 0111 
+              3 or more matches to the pattern would give 1111 
+        */
+        int numMatches = ppat->obsmarts.GetUMapList().size();       
+        int num =  ppat->numbits, div = ppat->numoccurrences+1, ngrp;
+ 
+        int i = n;
+        while(num)
+        {
+          ngrp = (num + div -1)/div--; //rounds up
+          num -= ngrp;
+          while(ngrp--)
+            numMatches > div ? SetBit(fp,i++) : i++;
+        }
+      }
+      n += ppat->numbits;
     }
 
-    if(nbits)
-      Fold(fp, nbits);
+    if(foldbits)
+      Fold(fp, foldbits);
     return true;
-  };
-
-  bool ReadPatternFile(const string& filename, vector<string>& lines)
+  }
+  
+  /////////////////////////////////////////////////////////////////////
+  bool ReadPatternFile()
   {
-    //Reads two types of file: SMARTS + comments and vice versa
-    //depending on whether the first line is #Comments after SMARTS
-    //Output strings in vector are SMARTS + comments
-    string file = filename;
+    //Reads three types of file. See below
     ifstream ifs;
-#ifdef HAVE_SSTREAM 
 	  stringstream errorMsg; 
-#else 
-	  strstream errorMsg; 
-#endif 
 
-    if (OpenDatafile(ifs, filename).length() == 0) {
-      errorMsg << "Cannot open " << filename << endl;
+    if (OpenDatafile(ifs, _patternsfile).length() == 0)
+    {
+      errorMsg << "Cannot open " << _patternsfile << endl;
       obErrorLog.ThrowError(__FUNCTION__, errorMsg.str(), obError);
       return false;
     }
 
-    if(!(ifs))
+    string line;
+    if(!getline(ifs, line)) //first line
+      return false;
+    bool smartsfirst = (Trim(line)=="#Comments after SMARTS");
+    
+    _bitcount=0;
+    while(ifs.good())
+    {
+      if( getline(ifs,line) && Trim(line).size() > 0 && line[0]!='#')
       {
-        errorMsg << "Cannot open " << filename << endl; 
-        obErrorLog.ThrowError(__FUNCTION__, errorMsg.str(), obError); 
-        return false;
-      }
-    string smarts, formatline;
+        pattern p;
+        p.numbits=1; p.numoccurrences=0; //default values
+        p.bitindex = _bitcount;
+        istringstream ss(line);
 
-    if(!getline(ifs, formatline)) return false;
-    if(Trim(formatline)=="#Comments after SMARTS")
-    {
-      while(ifs.good())
-      {
-        if( getline(ifs,smarts) 
-            && Trim(smarts).size() > 0 
-            && smarts[0] != '#')
-          lines.push_back(smarts); //leave the comments in
-      }
-    }
-    else
-    {
-      // Christian Laggner's format: SMARTS at end of line
-      while(ifs.good())
-      {
-        if( getline(ifs,smarts) && smarts[0]!='#')
+        if(smartsfirst)
         {
-          string::size_type pos = smarts.find(':');
-          if(pos!=string::npos)
+          if(isdigit(line[0]))
           {
-            pos = smarts.find_first_not_of(" \t", pos+1);
-            if(pos!=string::npos)
-            {
-              string buf = smarts.substr(pos);
-              lines.push_back(Trim(buf) + ' ' + smarts.substr(0,pos));
-            }
+            if(!ParseRDKitFormat(ss, p))
+              continue;
           }
+          else
+            //Original format, which looks like:
+            //  SMARTS description
+            ss >> p.smartsstring >> p.description;
         }
+        else
+        {
+          // Christian Laggner's format:
+          //  description: SMARTS [occurrences [numbits]]
+          getline(ss, p.description, ':');
+          ss >> p.smartsstring;
+          ss >> p.numoccurrences >> p.numbits;
+        }
+
+        if(!p.obsmarts.Init(p.smartsstring))
+        {
+          obErrorLog.ThrowError(__FUNCTION__,
+            "Faulty SMARTS: " + p.description + ' ' + p.smartsstring, obError);
+          continue;
+        }
+        _pats.push_back(p);
+        _bitcount += p.numbits;
       }
     }
-
+ 
     if (ifs)
       ifs.close();
     return true;
   }
 
-
-virtual string DescribeBits(const vector<unsigned int> fp, bool bSet=true)
-{
-  //checkmol-type output with tab separated functional group names
-  stringstream ss; 
-/*  ss << "out of possible " << smartsStrings.size();
-  if(!bSet)
-    ss << "\nUnset bits:";
-*/
-  for(int i=0; i<smartsStrings.size(); ++i)
+///////////////////////////////////////////////////////////////////////////////
+  virtual string DescribeBits(const vector<unsigned int> fp, bool bSet=true)
   {
-    if(GetBit(fp, i)==bSet)
+    //checkmol-type output with tab separated functional group names
+    stringstream ss;
+    vector<pattern>::iterator ppat;
+    for(ppat=_pats.begin();ppat!=_pats.end();++ppat)
     {
-      string::size_type pos = smartsStrings[i].find(' ');
-      if(pos!=string::npos)
-        ss << '\t' << smartsStrings[i].substr(pos+1);
+      int n = ppat->bitindex;
+      int num =  ppat->numbits, div = ppat->numoccurrences+1, ngrp;
+      while(num) //for each group of bits
+      {
+        ngrp = (num + div -1)/div--; //rounds up
+        num -= ngrp;
+        if(GetBit(fp, n) == bSet)
+        {
+          ss << '\t' << ppat->description;
+          if(div>0)
+            ss << '*' << div+1;
+          break; //ignore the bits signifying a smaller number of occurrences
+        }
+        n += ngrp;
+      }
     }
+    ss << endl;
+    return ss.str();
   }
-  ss << endl;
-  return ss.str();
-}
 
-};
+///////////////////////////////////////////////////////////////////////////////////
+  bool ParseRDKitFormat(istringstream& ss, pattern& p) 
+  {
+    //rdkit format, e.g.
+    //  14:('[S,s]-[S,s]',0), # S-S
+    const int dum = 20; //an arbitrary number in case delimiters in ignore statements not found
+    string number, comment;
+    getline(ss, number, ':');
+    ss.ignore(dum, '\'');
+    getline(ss, p.smartsstring, '\'');
+    if(p.smartsstring[0]=='?') //ignore patterns with SMARTS '?'
+      return false;
+    ss.ignore(dum,',');
+    ss >> p.numoccurrences;
+    ss.ignore(dum,'#');
+    getline(ss, comment);
+
+    //description is number + edited commment
+    Trim(comment);
+    string::size_type pos;
+    pos = comment.find("FIX");
+    if(pos==string::npos)
+      pos = comment.find("*NOTE*");
+    if(pos!=string::npos)
+      comment.erase(pos);
+    p.description = number + ": " + comment;
+    return true;
+  }
+
+
+}; //class PatternFP
+
 //***********************************************
 //Make a global instance
-PatternFP thePatternFP("FP3");
+PatternFP FP3PatternFP("FP3");
 
 PatternFP FP4PatternFP("FP4", "SMARTS_InteLigand.txt");
 //***********************************************
 
 /*! \class PatternFP
 A bit is set when there is a match to one of a list
-of SMARTS patterns in the file (default is patterns.txt).
-Looks for this file first in the folder in the environment variable
-BABEL_DATADIR, then in the folder specified by the macro BABEL_DATADIR
-(probably set during compilation in babelconfig.h), and then in the current folder.
+of SMARTS patterns in the datafile, which is specified in the constructor.
+If no filename is given, the default filename is patterns.txt.
+Fingerprints can be made by declaring a global variable, as in:
 
-On each line there is a SMARTS string and anything 
-after a space is ignored. Lines starting with # are ignored.
+PatternFP FP4PatternFP("FP4", "SMARTS_InteLigand.txt");
 
-Additional fingerprint types using patterns in different files
-can be made by just declaring separate instances like:
-\code
-PatternFP myPatternFP("myFP", "myPatternFile.txt");
-\endcode
+Alternatively, an entry in plugindefines.txt like:
+
+PatternFP
+MACCS          #ID of this fingerprint type
+MACCS.txt      #File containing the SMARTS patterns
+
+defines a fingerprint without the need to recompile.
+
+Three file formats are supported:
+ - the preferred format (e.g. SMARTS_InteLigand.txt in FP4)
+ - the original format (patterns.txt has an incomplete set of SMARTS patterns)
+ - a format made by extracting from an RDKit file (MACCS.txt)
+The last two require the first line to be:
+#Comments after SMARTS
+
+Lines starting with # are ignored.
+For the preferred format each line is of the form:
+description: SMARTS [occurrences [numbits]]
+A bit is set in the fingerprint for ach SMARTS pattern matched.
+The optional integer parameters refine this behaviour; the most obvious uses are:
+ - if <occurrences> is present and greater than its default value of 0, the bit
+   is set only if the number of matches to the pattern is greater than <occurences>.
+ - if <occurences> is 0 and <numbits> is greater than its default value of 1, then
+   the fingerprint has <numbits> bits set if there is a match. This gives greater weight
+   to the pattern for use in similarity measures like Tanimoto.
+ - if the parameters are n-1 and n and the number of matches is n,
+   a bit is set for each of the conditions n>=m, n>=m-1, ... , n>=1
+   This can be used to distinguish structures with many similar atoms like n-alkanes.
+The use of other values for the parameters, which can be any positive integer, can give
+other analogous behaviours. If numbits is 0 the pattern is ignored.
 */
 
 }//namespace
 
 //! \file finger3.cpp
-//! \brief fingerprint3 based on list of SMARTS patterns
+//! \brief fingerprints based on list of SMARTS patterns
