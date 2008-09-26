d836b68516b3cdea1bb5a47ca28785ec4f035ed1
Noel O'Boyle <baoilleach@gmail.com>
2008-09-26 10:24:59 +0000

    2008-09-26  Noel O'Boyle  <baoilleach@gmail.com>
    	* windows-vc2005/OBCSharp/obdotnet.bat: Update the CSharp build so
    	that it automatically creates a distribution for release
    	* windows-vc2005/OBCSharp/IronPython_Instructions.txt: How to use the
    	OBDotNet distribution from IronPython

--- windows-vc2005/OBCSharp/obdotnet.bat
+++ windows-vc2005/OBCSharp/obdotnet.bat
@@ -1,2 +1,11 @@
+set ver=0.1
 %CSHARP%/Csc.exe /target:library /optimize /out:OBDotNet.dll *.cs
 del /Q *.cs
+set dist=OBDotNet-%ver%
+rmdir /s /q %dist%
+mkdir %dist%
+copy OBDotNet.dll %dist%
+copy openbabel.dll %dist%
+copy IronPython_Instructions.txt %dist%
+copy ..\*.obf %dist%
+copy ..\*.dll %dist%
