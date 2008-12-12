openbabel/openbabel

commit c59702047a6c550457ac4b6e55ab898bcef9e551

Fixes for PR#1800968 and 1800694 -- incorrect Sybyl atom types.
git-svn-id: https://openbabel.svn.sourceforge.net/svnroot/openbabel/openbabel/trunk@2578 71e0b675-c112-0410-beda-96664073b5c2

src/typer.cpp
@@ -171,6 +171,27 @@ namespace OpenBabel
           for (j = _mlist.begin();j != _mlist.end();++j)
             mol.GetAtom((*j)[0])->SetType(i->second);
         }
+
+    // Special cases
+    vector<OBAtom*>::iterator a;
+    OBAtom* atom;
+    for (atom = mol.BeginAtom(a); atom; atom = mol.NextAtom(a)) {
+      // guanidinium. Fixes PR#1800964
+      if (strncasecmp(atom->GetType(),"C2", 2) == 0) {
+        int guanidineN = 0;
+        OBAtom *nbr;
+        vector<OBBond*>::iterator k;
+        for (nbr = atom->BeginNbrAtom(k);nbr;nbr = atom->NextNbrAtom(k)) {
+          if (strncasecmp(nbr->GetType(),"Npl", 3) == 0 ||
+              strncasecmp(nbr->GetType(),"N2", 2) == 0 ||
+              strncasecmp(nbr->GetType(),"Ng+", 3) == 0)
+            ++guanidineN;
+        }
+        atom->SetType("C+");
+
+      } // end C2 carbon for guanidinium
+
+    } // end special cases
   }
 
   void OBAtomTyper::AssignHyb(OBMol &mol)

data/atomtyp.h
@@ -159,79 +159,79 @@ static const char AtomTypeData[] = {
  0x0A,0x49,0x4D,0x50,0x56,0x41,0x4C,0x20,0x5B,0x61,0x73,0x5D,0x09,0x09,0x09,
  0x33,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x37,0x5E,0x33,0x5D,
  0x09,0x09,0x09,0x09,0x09,0x4E,0x33,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,
- 0x5B,0x23,0x37,0x58,0x34,0x2B,0x5D,0x09,0x09,0x09,0x09,0x09,0x4E,0x33,0x2B,
- 0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x37,0x5E,0x32,0x5D,0x09,
- 0x09,0x09,0x09,0x09,0x4E,0x70,0x6C,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,
- 0x5B,0x23,0x37,0x5E,0x31,0x5D,0x09,0x09,0x09,0x09,0x09,0x4E,0x31,0x0A,0x45,
- 0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x4E,0x3D,0x2A,0x29,0x5D,0x09,
- 0x09,0x09,0x09,0x4E,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,
- 0x28,0x5B,0x4E,0x72,0x30,0x5D,0x43,0x28,0x3D,0x5B,0x4E,0x72,0x30,0x5D,0x29,
- 0x5B,0x4E,0x72,0x30,0x5D,0x29,0x5D,0x09,0x09,0x4E,0x67,0x2B,0x0A,0x45,0x58,
- 0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x5B,0x4E,0x72,0x30,0x5D,0x3D,0x43,
- 0x28,0x5B,0x4E,0x72,0x30,0x5D,0x29,0x5B,0x4E,0x72,0x30,0x5D,0x29,0x5D,0x09,
+ 0x5B,0x23,0x37,0x58,0x34,0x5D,0x09,0x09,0x09,0x09,0x09,0x4E,0x33,0x2B,0x0A,
+ 0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x37,0x5E,0x32,0x5D,0x09,0x09,
+ 0x09,0x09,0x09,0x4E,0x70,0x6C,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,
+ 0x23,0x37,0x5E,0x31,0x5D,0x09,0x09,0x09,0x09,0x09,0x4E,0x31,0x0A,0x45,0x58,
+ 0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x4E,0x3D,0x2A,0x29,0x5D,0x09,0x09,
+ 0x09,0x09,0x4E,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,
+ 0x5B,0x4E,0x72,0x30,0x5D,0x43,0x28,0x3D,0x5B,0x4E,0x72,0x30,0x5D,0x29,0x5B,
+ 0x4E,0x72,0x30,0x5D,0x29,0x5D,0x09,0x09,0x4E,0x67,0x2B,0x0A,0x45,0x58,0x54,
+ 0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x5B,0x4E,0x72,0x30,0x5D,0x3D,0x43,0x28,
+ 0x5B,0x4E,0x72,0x30,0x5D,0x29,0x5B,0x4E,0x72,0x30,0x5D,0x29,0x5D,0x09,0x09,
+ 0x4E,0x67,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x5B,
+ 0x4E,0x44,0x31,0x5D,0x3D,0x43,0x5B,0x4E,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,
  0x09,0x4E,0x67,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,
- 0x5B,0x4E,0x44,0x31,0x5D,0x3D,0x43,0x5B,0x4E,0x44,0x31,0x5D,0x29,0x5D,0x09,
+ 0x5B,0x4E,0x44,0x31,0x5D,0x43,0x3D,0x5B,0x4E,0x44,0x31,0x5D,0x29,0x5D,0x09,
  0x09,0x09,0x4E,0x67,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,
- 0x28,0x5B,0x4E,0x44,0x31,0x5D,0x43,0x3D,0x5B,0x4E,0x44,0x31,0x5D,0x29,0x5D,
- 0x09,0x09,0x09,0x4E,0x67,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,
- 0x24,0x28,0x4E,0x28,0x4F,0x29,0x2A,0x29,0x5D,0x09,0x09,0x09,0x09,0x4E,0x6F,
- 0x78,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x4E,0x43,0x3D,
- 0x5B,0x4F,0x2C,0x53,0x5D,0x29,0x5D,0x09,0x09,0x09,0x09,0x4E,0x61,0x6D,0x0A,
- 0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x4E,0x28,0x7E,0x5B,0x4F,
- 0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,0x09,
- 0x4E,0x74,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x6E,0x5D,0x09,
- 0x09,0x09,0x09,0x09,0x4E,0x61,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,
- 0x5B,0x23,0x36,0x5E,0x33,0x5D,0x09,0x09,0x09,0x09,0x09,0x43,0x33,0x0A,0x45,
- 0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x36,0x5E,0x32,0x5D,0x09,0x09,0x09,
- 0x09,0x09,0x43,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,
- 0x43,0x28,0x3D,0x4F,0x29,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,0x09,
- 0x09,0x43,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x63,0x5D,
- 0x09,0x09,0x09,0x09,0x09,0x43,0x61,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,
- 0x09,0x5B,0x23,0x36,0x5E,0x31,0x5D,0x09,0x09,0x09,0x09,0x09,0x43,0x31,0x0A,
- 0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x36,0x2B,0x5D,0x09,0x09,0x09,
- 0x09,0x09,0x43,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x36,
- 0x2D,0x5D,0x09,0x09,0x09,0x09,0x09,0x43,0x2D,0x0A,0x45,0x58,0x54,0x54,0x59,
- 0x50,0x09,0x5B,0x23,0x38,0x5E,0x33,0x5D,0x09,0x09,0x09,0x09,0x09,0x4F,0x33,
- 0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x38,0x5E,0x32,0x5D,0x09,
- 0x09,0x09,0x09,0x09,0x4F,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x20,0x5B,
- 0x24,0x28,0x4F,0x61,0x29,0x5D,0x09,0x09,0x09,0x09,0x09,0x4F,0x32,0x0A,0x45,
- 0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x5B,0x4F,0x44,0x32,0x5D,0x43,
- 0x3D,0x5B,0x4F,0x2C,0x53,0x5D,0x29,0x5D,0x09,0x09,0x09,0x4F,0x33,0x09,0x23,
- 0x65,0x73,0x74,0x65,0x72,0x20,0x6F,0x78,0x79,0x67,0x65,0x6E,0x0A,0x45,0x58,
- 0x54,0x54,0x59,0x50,0x20,0x5B,0x24,0x28,0x4F,0x61,0x29,0x5D,0x09,0x09,0x09,
- 0x09,0x09,0x4F,0x33,0x20,0x23,0x61,0x72,0x6F,0x6D,0x61,0x74,0x69,0x63,0x20,
- 0x65,0x74,0x68,0x65,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,
- 0x38,0x2D,0x5D,0x09,0x09,0x09,0x09,0x09,0x4F,0x2D,0x0A,0x45,0x58,0x54,0x54,
- 0x59,0x50,0x20,0x5B,0x24,0x28,0x5B,0x23,0x38,0x44,0x31,0x5D,0x7E,0x5B,0x23,
- 0x36,0x5D,0x7E,0x5B,0x23,0x38,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,0x09,0x4F,
- 0x2E,0x63,0x6F,0x32,0x20,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,
- 0x31,0x35,0x5D,0x09,0x09,0x09,0x09,0x09,0x50,0x0A,0x45,0x58,0x54,0x54,0x59,
- 0x50,0x09,0x5B,0x23,0x31,0x35,0x58,0x34,0x2B,0x5D,0x09,0x09,0x09,0x09,0x50,
- 0x33,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,0x35,0x3B,
- 0x24,0x28,0x5B,0x23,0x31,0x35,0x44,0x34,0x5D,0x28,0x7E,0x5B,0x4F,0x44,0x31,
- 0x5D,0x29,0x28,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,
- 0x5D,0x29,0x5D,0x09,0x50,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x20,
- 0x5B,0x23,0x31,0x35,0x3B,0x24,0x28,0x5B,0x23,0x31,0x35,0x44,0x33,0x5D,0x28,
+ 0x28,0x4E,0x28,0x4F,0x29,0x2A,0x29,0x5D,0x09,0x09,0x09,0x09,0x4E,0x6F,0x78,
+ 0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x4E,0x43,0x3D,0x5B,
+ 0x4F,0x2C,0x53,0x5D,0x29,0x5D,0x09,0x09,0x09,0x09,0x4E,0x61,0x6D,0x0A,0x45,
+ 0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x4E,0x28,0x7E,0x5B,0x4F,0x44,
+ 0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,0x09,0x4E,
+ 0x74,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x6E,0x5D,0x09,0x09,
+ 0x09,0x09,0x09,0x4E,0x61,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,
+ 0x23,0x36,0x5E,0x33,0x5D,0x09,0x09,0x09,0x09,0x09,0x43,0x33,0x0A,0x45,0x58,
+ 0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x36,0x5E,0x32,0x5D,0x09,0x09,0x09,0x09,
+ 0x09,0x43,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x43,
+ 0x28,0x3D,0x4F,0x29,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,0x09,0x09,
+ 0x43,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x63,0x5D,0x09,
+ 0x09,0x09,0x09,0x09,0x43,0x61,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,
+ 0x5B,0x23,0x36,0x5E,0x31,0x5D,0x09,0x09,0x09,0x09,0x09,0x43,0x31,0x0A,0x45,
+ 0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x36,0x2B,0x5D,0x09,0x09,0x09,0x09,
+ 0x09,0x43,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x36,0x2D,
+ 0x5D,0x09,0x09,0x09,0x09,0x09,0x43,0x2D,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,
+ 0x09,0x5B,0x23,0x38,0x5E,0x33,0x5D,0x09,0x09,0x09,0x09,0x09,0x4F,0x33,0x0A,
+ 0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x38,0x5E,0x32,0x5D,0x09,0x09,
+ 0x09,0x09,0x09,0x4F,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x20,0x5B,0x24,
+ 0x28,0x4F,0x61,0x29,0x5D,0x09,0x09,0x09,0x09,0x09,0x4F,0x32,0x0A,0x45,0x58,
+ 0x54,0x54,0x59,0x50,0x09,0x5B,0x24,0x28,0x5B,0x4F,0x44,0x32,0x5D,0x43,0x3D,
+ 0x5B,0x4F,0x2C,0x53,0x5D,0x29,0x5D,0x09,0x09,0x09,0x4F,0x33,0x09,0x23,0x65,
+ 0x73,0x74,0x65,0x72,0x20,0x6F,0x78,0x79,0x67,0x65,0x6E,0x0A,0x45,0x58,0x54,
+ 0x54,0x59,0x50,0x20,0x5B,0x24,0x28,0x4F,0x61,0x29,0x5D,0x09,0x09,0x09,0x09,
+ 0x09,0x4F,0x33,0x20,0x23,0x61,0x72,0x6F,0x6D,0x61,0x74,0x69,0x63,0x20,0x65,
+ 0x74,0x68,0x65,0x72,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x38,
+ 0x2D,0x5D,0x09,0x09,0x09,0x09,0x09,0x4F,0x2D,0x0A,0x45,0x58,0x54,0x54,0x59,
+ 0x50,0x20,0x5B,0x24,0x28,0x5B,0x23,0x38,0x44,0x31,0x5D,0x7E,0x5B,0x23,0x36,
+ 0x5D,0x7E,0x5B,0x23,0x38,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,0x09,0x4F,0x2E,
+ 0x63,0x6F,0x32,0x20,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,
+ 0x35,0x5D,0x09,0x09,0x09,0x09,0x09,0x50,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,
+ 0x09,0x5B,0x23,0x31,0x35,0x58,0x34,0x2B,0x5D,0x09,0x09,0x09,0x09,0x50,0x33,
+ 0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,0x35,0x3B,0x24,
+ 0x28,0x5B,0x23,0x31,0x35,0x44,0x34,0x5D,0x28,0x7E,0x5B,0x4F,0x44,0x31,0x5D,
+ 0x29,0x28,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,
+ 0x29,0x5D,0x09,0x50,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x20,0x5B,
+ 0x23,0x31,0x35,0x3B,0x24,0x28,0x5B,0x23,0x31,0x35,0x44,0x33,0x5D,0x28,0x7E,
+ 0x5B,0x4F,0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,0x09,
+ 0x09,0x50,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,
+ 0x36,0x5D,0x09,0x09,0x09,0x09,0x09,0x53,0x33,0x0A,0x45,0x58,0x54,0x54,0x59,
+ 0x50,0x09,0x5B,0x23,0x31,0x36,0x44,0x33,0x2B,0x2C,0x23,0x31,0x36,0x58,0x33,
+ 0x2B,0x5D,0x09,0x09,0x09,0x09,0x53,0x33,0x2B,0x0A,0x45,0x58,0x54,0x54,0x59,
+ 0x50,0x09,0x5B,0x23,0x31,0x36,0x3B,0x73,0x2C,0x24,0x28,0x5B,0x53,0x44,0x31,
+ 0x5D,0x3D,0x2A,0x29,0x5D,0x09,0x09,0x09,0x53,0x32,0x0A,0x45,0x58,0x54,0x54,
+ 0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x44,0x34,0x3B,0x24,0x28,0x53,0x28,0x3D,
+ 0x4F,0x29,0x28,0x3D,0x4F,0x29,0x29,0x5D,0x09,0x09,0x09,0x53,0x6F,0x32,0x0A,
+ 0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x44,0x33,0x3B,0x24,
+ 0x28,0x53,0x28,0x3D,0x4F,0x29,0x29,0x5D,0x09,0x09,0x09,0x53,0x6F,0x78,0x0A,
+ 0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x3B,0x24,0x28,0x5B,
+ 0x23,0x31,0x36,0x44,0x34,0x5D,0x28,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x28,
  0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,
- 0x09,0x09,0x50,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,
- 0x31,0x36,0x5D,0x09,0x09,0x09,0x09,0x09,0x53,0x33,0x0A,0x45,0x58,0x54,0x54,
- 0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x44,0x33,0x2B,0x2C,0x23,0x31,0x36,0x58,
- 0x33,0x2B,0x5D,0x09,0x09,0x09,0x09,0x53,0x33,0x2B,0x0A,0x45,0x58,0x54,0x54,
- 0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x3B,0x73,0x2C,0x24,0x28,0x5B,0x53,0x44,
- 0x31,0x5D,0x3D,0x2A,0x29,0x5D,0x09,0x09,0x09,0x53,0x32,0x0A,0x45,0x58,0x54,
- 0x54,0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x44,0x34,0x3B,0x24,0x28,0x53,0x28,
- 0x3D,0x4F,0x29,0x28,0x3D,0x4F,0x29,0x29,0x5D,0x09,0x09,0x09,0x53,0x6F,0x32,
- 0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x44,0x33,0x3B,
- 0x24,0x28,0x53,0x28,0x3D,0x4F,0x29,0x29,0x5D,0x09,0x09,0x09,0x53,0x6F,0x78,
- 0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,0x36,0x3B,0x24,0x28,
- 0x5B,0x23,0x31,0x36,0x44,0x34,0x5D,0x28,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,
- 0x28,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,
- 0x5D,0x09,0x53,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,
- 0x31,0x36,0x3B,0x24,0x28,0x5B,0x23,0x31,0x36,0x44,0x33,0x5D,0x28,0x7E,0x5B,
- 0x4F,0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,
- 0x53,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x35,0x5D,
- 0x09,0x09,0x09,0x09,0x09,0x42,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,
- 0x5B,0x23,0x35,0x44,0x34,0x5D,0x09,0x09,0x09,0x09,0x09,0x42,0x33,0x0A,0x00};
+ 0x09,0x53,0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x31,
+ 0x36,0x3B,0x24,0x28,0x5B,0x23,0x31,0x36,0x44,0x33,0x5D,0x28,0x7E,0x5B,0x4F,
+ 0x44,0x31,0x5D,0x29,0x7E,0x5B,0x4F,0x44,0x31,0x5D,0x29,0x5D,0x09,0x09,0x53,
+ 0x61,0x63,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,0x23,0x35,0x5D,0x09,
+ 0x09,0x09,0x09,0x09,0x42,0x32,0x0A,0x45,0x58,0x54,0x54,0x59,0x50,0x09,0x5B,
+ 0x23,0x35,0x44,0x34,0x5D,0x09,0x09,0x09,0x09,0x09,0x42,0x33,0x0A,0x00};
 
 } // namespace OpenBabel
 #endif // OB_ATOMTYP_H

commit 1b3436f7291fb007404c0671676a4c3e56092a08

* src/typer.cpp: Fix for PR#2017902 -- bug introduced by r2578 in         detecting guanidinium C atoms.

src/typer.cpp
@@ -187,7 +187,8 @@ namespace OpenBabel
               strncasecmp(nbr->GetType(),"Ng+", 3) == 0)
             ++guanidineN;
         }
-        atom->SetType("C+");
+        if (guanidineN == 3)
+          atom->SetType("C+");
 
       } // end C2 carbon for guanidinium
 