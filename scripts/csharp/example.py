4501147825576a4bef3d3c63f24dd3fc36c08aaf
Geoffrey Hutchison <geoff@geoffhutchison.net>
2009-01-13 17:02:27 +0000

    Makefile fixes for the script bindings.

--- scripts/csharp/example.py
+++
@@ -1,8 +0,0 @@
-import clr
-clr.AddReference("OBDotNet.dll")
-import OpenBabel as ob
-conv = ob.OBConversion()
-conv.SetInFormat("smi")
-mol = ob.OBMol()
-conv.ReadString(mol, "CC=O")
-print mol.GetMolWt()
