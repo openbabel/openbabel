db0571080ccf9c17a1d089781363aeaa6ee163c2
Michael Banck <mbanck@gmx.net>
2009-04-06 22:22:01 +0000

    2009-04-07  Michael Banck  <mbanck@gmx.net>
            * src/formats/moldenformat.cpp (ReadMolecule): Added frequencies
            and IR intensities support.  Closes PR#2737715.

--- src/formats/moldenformat.cpp
+++ src/formats/moldenformat.cpp
@@ -1,6 +1,7 @@
 //
 // Molekel - Molecular Visualization Program
 // Copyright (C) 2006, 2007 Swiss National Supercomputing Centre (CSCS)
+// Some portions Copyright (C) 2009 Michael Banck
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -109,37 +110,103 @@ bool OBMoldenFormat::ReadMolecule( OBBase* pOb, OBConversion* pConv )
 
     istream& ifs = *pConv->GetInStream();
 
+    //Vibrational data
+    std::vector< std::vector< vector3 > > Lx;
+    std::vector<double> Frequencies, Intensities;
+
     pmol->BeginModify();
     pmol->SetDimension( 3 );
     string lineBuffer;
-    getline( ifs, lineBuffer );
 
-    while( ifs && lineBuffer.find( "[Atoms]" ) == string::npos )
-    {
-        getline( ifs, lineBuffer );
+    while( getline( ifs, lineBuffer ) )
+      {
+        if( lineBuffer.find( "[Atoms]" ) != string::npos || 
+            lineBuffer.find( "[ATOMS]" ) != string::npos ) {
+          double factor = 1.; // Angstrom
+          if( lineBuffer.find( "AU" ) != string::npos ) factor = 0.529177249; // Bohr
+          getline( ifs, lineBuffer );
+          while( lineBuffer.find( "[") == string::npos )
+            {
+              if( lineBuffer == "" ) continue;
+              istringstream is( lineBuffer );
+              string atomName;
+              int atomId;
+              int atomicNumber;
+              double x, y, z;
+              is >> atomName >> atomId >> atomicNumber >> x >> y >> z;
+              OBAtom* atom = pmol->NewAtom();
+              if( !atom ) break;
+              atom->SetAtomicNum( atomicNumber );
+              atom->SetVector( x * factor, y * factor, z * factor );
+              getline( ifs, lineBuffer );
+            }
+	  continue;
+        } // "[Atoms]" || "[ATOMS]"
+        if( lineBuffer.find( "[FREQ]" ) != string::npos ) {        
+          while( getline( ifs, lineBuffer ) )
+            {
+              if( lineBuffer == "" ) continue;
+              if( lineBuffer.find( "[" ) != string::npos ) break;
+              istringstream is( lineBuffer );
+              double freq;
+              is >> freq;
+              Frequencies.push_back( freq );
+            }
+          continue;
+        } // "[FREQ]"
+        if( lineBuffer.find( "[INT]" ) != string::npos ) {        
+          while( getline( ifs, lineBuffer ) )
+            {
+              if( lineBuffer == "" ) continue;
+              if( lineBuffer.find( "[" ) != string::npos ) break;
+              istringstream is( lineBuffer );
+              double intens;
+              is >> intens;
+              Intensities.push_back( intens );
+            }
+          continue;
+        } // "[INT]"
+        if( lineBuffer.find( "[FR-NORM-COORD]" ) != string::npos ) {
+          getline( ifs, lineBuffer );
+          while( ifs && lineBuffer.find( "Vibration") != string::npos ) 
+            {
+              vector<vector3> vib;
+              getline( ifs, lineBuffer );
+              while( ifs && lineBuffer.find( "Vibration") == string::npos ) 
+                {
+                  istringstream is( lineBuffer );
+                  double x, y, z;
+                  is >> x >> y >> z;
+                  vib.push_back( vector3( x, y, z ) );
+                  getline( ifs, lineBuffer );
+                }
+              Lx.push_back( vib );  
+           } // while
+        } // "[FR-NORM-COORD]"
+      } // while
+
+    if ( pmol->NumAtoms() == 0 ) {
+      pmol->EndModify();
+      return false;
     }
 
-    if( !ifs ) return false;
-
-    //[Atoms] AU OR Angs
-    double factor = 1.; // Angstrom
-    if( lineBuffer.find( "AU" ) != string::npos ) factor = 0.529177249; // Bohr
-
-    while( ifs )
+    // Attach vibrational data, if there is any, to molecule
+    if(Frequencies.size()>0)
     {
-        getline( ifs, lineBuffer );
-        if( lineBuffer.size() == 0 ) continue;
-        if( lineBuffer.find( '[' ) != string::npos ) break;
-        istringstream is( lineBuffer );
-        string atomName;
-        int atomId;
-        int atomicNumber;
-        double x, y, z;
-        is >> atomName >> atomId >> atomicNumber >> x >> y >> z;
-        OBAtom* atom = pmol->NewAtom();
-        if( !atom ) break;
-        atom->SetAtomicNum( atomicNumber );
-        atom->SetVector( x * factor, y * factor, z * factor );
+      for (int i=0; i < Frequencies.size(); i++) {
+        // Set intensities to zero if none were specified
+        if (Intensities.size() < Frequencies.size() ) Intensities.push_back( 0.0 );
+        if (fabs(Frequencies[i]) < 10.) {
+          // skip translational and rotational modes
+          Frequencies.erase( Frequencies.begin() + i );
+          Intensities.erase( Intensities.begin() + i );
+          Lx.erase( Lx.begin() + i );  
+          i--;  // compensate for the vibration which just got cut out
+        }
+      }
+      OBVibrationData* vd = new OBVibrationData;
+      vd->SetData(Lx, Frequencies, Intensities);
+      pmol->SetData(vd);
     }
 
     if( !pConv->IsOption( "b", OBConversion::INOPTIONS ) ) pmol->ConnectTheDots();
