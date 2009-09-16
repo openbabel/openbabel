2db5aa69a3054d3df6a6a0966fd81f37c71fd754
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2009-09-16 11:05:07 +0000

    ontest.h: Add OB_COMPARE

--- test/obtest.h
+++ test/obtest.h
@@ -8,8 +8,18 @@
       exit(-1);
 }
 
+template <typename T1, typename T2>
+void ob_compare(T1 a, T2 b, const char *file, int line, const char *func_name)
+{
+  if (!(a == b))
+    std::cout << file << ":" << line << ": " << a << " == " << b << " (FAIL)" << std::endl; 
+}
+
 #define OB_ASSERT(exp) \
   ( (exp) ? static_cast<void>(0) : report_error(#exp, __FILE__, __LINE__, __PRETTY_FUNCTION__, false) ) 
 
 #define OB_REQUIRE(exp) \
   ( (exp) ? static_cast<void>(0) : report_error(#exp, __FILE__, __LINE__, __PRETTY_FUNCTION__, true) ) 
+
+#define OB_COMPARE(a,b) \
+  ob_compare(a, b, __FILE__, __LINE__, __PRETTY_FUNCTION__)
