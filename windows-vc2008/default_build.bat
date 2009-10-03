740c83f765d7beea697b4e4f980b789c3e354d98
Noel O'Boyle <baoilleach@gmail.com>
2009-10-03 18:28:43 +0000

    * Update Windows build to use the InChI dll, and to build everything 	to the same folder (e.g. Release or Debug). Tests can be run in Debug 	build

--- windows-vc2008/default_build.bat
+++ windows-vc2008/default_build.bat
@@ -1,6 +1,6 @@
-@rem -DMINIMAL_BUILD=OFF -DBUILD_GUI=ON -DENABLE_TESTS=OFF
+@rem -DMINIMAL_BUILD=OFF -DBUILD_GUI=ON -DENABLE_TESTS=ON
 
 @if NOT EXIST build mkdir build
 @cd build
-"C:\Program Files\CMake 2.6\bin\cmake.exe" -DCMAKE_INSTALL_PREFIX=..\install -G "Visual Studio 9 2008" -DLIBXML2_LIBRARIES=..\..\windows-vc2005\libxml2.lib -DLIBXML2_INCLUDE_DIR=. -DZLIB_LIBRARY=..\..\windows-vc2005\zlib1.lib -DZLIB_INCLUDE_DIR=. %1 %2 %3 %4 %5 %6 %7 %8 ..\..
+"C:\Program Files\CMake 2.6\bin\cmake.exe" -DCMAKE_INSTALL_PREFIX=..\install -G "Visual Studio 9 2008" -DLIBXML2_LIBRARIES=..\..\windows-vc2005\libxml2.lib -DLIBXML2_INCLUDE_DIR=. -DZLIB_LIBRARY=..\..\windows-vc2005\zlib1.lib -DZLIB_INCLUDE_DIR=. -DENABLE_TESTS=ON %1 %2 %3 %4 %5 %6 %7 %8 ..\..
 @cd ..
