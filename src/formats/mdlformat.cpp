5418424e5996e8490f1b04f64c987e6e52024173
Chris Morley <c.morley@gaseq.co.uk>
2010-02-10 10:11:16 +0000

    MDLFormat: allow numbers to be left justified.

--- src/formats/mdlformat.cpp
+++ src/formats/mdlformat.cpp
@@ -1388,7 +1388,7 @@ namespace OpenBabel
     char *end;
     if (s == NULL) return 0;
     int n = strtol(s, &end, 10);
-    if (*end != '\0') return 0;
+    if (*end != '\0' && *end != ' ') return 0;
     return n;
   }
 
@@ -1397,7 +1397,7 @@ namespace OpenBabel
     char *end;
     if (s == NULL) return 0;
     int n = strtoul(s, &end, 10);
-    if (*end != '\0') return 0;
+    if (*end != '\0' && *end != ' ') return 0;
     return n;
   }
 }
