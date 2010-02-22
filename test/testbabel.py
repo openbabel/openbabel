37f083556e59fc6fd0356f71d1ebb85f7832be63
Noel O'Boyle <baoilleach@gmail.com>
2010-02-22 22:29:48 +0000

    2010-02-22  Noel O'Boyle
    	Enable testing of OpenBabel executables from Python
    	* test/testbabel.py: Initial checkin of tests for the Babel executable
    	* test/CMakeLists.txt: Add testbabel.py
    	* cmake/modules/UsePythonTest.cmake: Add a CMake module for adding
    	Python tests (BSD License) from Mathieu Malaterre

---
+++ test/testbabel.py
@@ -0,0 +1,69 @@
+"""Test OpenBabel executables from Python
+
+Note: Python bindings not used
+
+On Windows or Linux, you can run these tests at the commandline
+in the build folder with:
+"C:\Program Files\CMake 2.6\bin\ctest.exe" -C CTestTestfile.cmake
+                                           -R testpy -VV
+
+You could also "chdir" into build/test and run the test file directly:
+python ../../test/testbabel.py
+
+In both cases, the test file is run directly from the source folder,
+and so you can quickly develop the tests and try them out.
+"""
+
+import os
+import sys
+import unittest
+
+from subprocess import Popen, PIPE
+
+def run_exec(text, commandline):
+    """Pipe from stdin to an executable
+
+    Example: run_exec("CC(=O)Cl", "babel -ismi -oinchi")
+
+    Return a tuple (stdout, stderr)
+    """
+    broken = commandline.split()
+    exe = executable(broken[0])
+    p = Popen([exe] + broken[1:], 
+              stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate(text)
+    return stdout, stderr
+
+def executable(name):
+    """Return the full path to an executable"""
+    suffix = ""
+    folder = "bin"
+    if sys.platform == "win32":
+        suffix = ".exe"
+        folder = "Release"
+    return os.path.join("..", folder, name + suffix)
+
+def log(text):
+    """Convenience function for debugging tests
+
+    The log file (log.txt) is created in build/test
+    """
+    output = open("log.txt", "a")
+    print >> output, text
+    output.close()
+
+class testBabel(unittest.TestCase):
+    """A series of tests relating to the Babel executable"""
+
+    def canFindExecutable(self, name):
+        fullpath = executable(name)
+        self.assertTrue(os.path.isfile(fullpath),
+                        "'%s' executable not found at %s" % (name, fullpath))
+        
+    def testSMItoInChI(self):
+        self.canFindExecutable("babel")
+        output, error = run_exec("CC(=O)Cl", "babel -ismi -oinchi")
+        self.assertEqual(output.rstrip(), "InChI=1S/C2H3ClO/c1-2(3)4/h1H3")
+
+if __name__ == "__main__":
+    unittest.main()
