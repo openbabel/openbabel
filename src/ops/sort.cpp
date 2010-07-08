85733eb05c57ada2ab1171f744aba2f0675b3fe8
Geoffrey Hutchison <geoff@geoffhutchison.net>
2010-07-08 12:52:54 +0000

    Fix compile problem on Linux/Mac with GCC and Clang. Can't pass a temporary stream as a parameter.

--- src/ops/sort.cpp
+++ src/ops/sort.cpp
@@ -140,8 +140,8 @@ bool OpSort::Do(OBBase* pOb, const char* OptionText, OpMap* pmap, OBConversion* 
     if(_addDescToTitle)
       *const_cast<char*>(pLast)='\0';
 
-    std::pair<std::string,std::string> spair
-             = OBDescriptor::GetIdentifier(std::istringstream(OptionText));
+    std::istringstream optionStream(OptionText);
+    std::pair<std::string,std::string> spair = OBDescriptor::GetIdentifier(optionStream);
     _pDesc = OBDescriptor::FindType(spair.first.c_str());
     if(!_pDesc)
     {
