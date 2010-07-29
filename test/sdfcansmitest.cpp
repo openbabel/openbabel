575f7bfc546539aa7d52092d9aad8b54533edc4b
Geoffrey Hutchison <geoff@geoffhutchison.net>
2010-07-29 02:43:49 +0000

    * test/sdfcansmitest.cpp: Added new unit test suggested by TJ         O'Donnell to check for canonical SMILES consistency from a         SDFile. Currently shows 4 / 48 failures.

---
+++ test/sdfcansmitest.cpp
@@ -0,0 +1,82 @@
+#include "obtest.h"
+#include <openbabel/mol.h>
+#include <openbabel/obconversion.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using namespace OpenBabel;
+
+std::string GetFilename(const std::string &filename)
+{
+  std::string path = TESTDATADIR + filename;
+  return path;
+}
+
+static unsigned int failed = 0;
+static unsigned int testCount = 0;
+
+bool doMultiMoleculeFile(const std::string &filename)
+{
+  std::string file = GetFilename(filename);
+  std::ifstream ifs;
+  ifs.open(file.c_str());
+  OB_REQUIRE( ifs );
+
+  OBMol mol;
+  OBConversion conv(&ifs, &cout);
+  OBFormat *format = conv.FormatFromExt(file.c_str());
+  OBFormat *canSMI = conv.FindFormat("can");
+  OBFormat *smi    = conv.FindFormat("smi");
+  OB_REQUIRE(format);
+  OB_REQUIRE(canSMI);
+  OB_REQUIRE(smi);
+
+  OB_REQUIRE(conv.SetInAndOutFormats(format, canSMI)); 
+
+  string output, roundtrip; // first output string, then the roundtrip
+  OBMol round2; // result of reading first output as canonical SMILES
+  OBConversion conv2; // duplicate to prevent having to constantly change formats
+  OB_REQUIRE(conv2.SetInAndOutFormats(smi, canSMI));
+
+  bool result = true;
+  while (conv.Read(&mol, &ifs)) {
+    testCount++;
+
+    mol.SetTitle("");
+    output = conv.WriteString(&mol, true); // trim whitespace
+    OB_REQUIRE(conv2.ReadString(&round2, output));
+    round2.SetTitle("");
+    roundtrip = conv2.WriteString(&round2, true);
+    if (roundtrip != output) {
+      failed++;
+      result = false;
+      cout << "Failed roundtrip: \n  " << output << " -> \n  " << roundtrip << endl;
+    }
+  }
+
+  return result;
+}
+
+
+int main(int argc, char **argv)
+{
+  // Define location of file formats for testing
+  #ifdef FORMATDIR
+    char env[BUFF_SIZE];
+    snprintf(env, BUFF_SIZE, "BABEL_LIBDIR=%s", FORMATDIR);
+    putenv(env);
+  #endif  
+
+  OB_ASSERT( doMultiMoleculeFile("forcefield.sdf") );
+  OB_ASSERT( doMultiMoleculeFile("filterset.sdf") );
+  OB_ASSERT( doMultiMoleculeFile("cantest.sdf") );
+
+  cout << "PASSED TESTS: " << testCount - failed << "/" << testCount << endl;
+
+  return 0;
+}
+
