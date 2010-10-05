514d4cc5d8154896e6deba48444fdaa83d5cb89e
Noel O'Boyle <baoilleach@gmail.com>
2010-10-05 13:43:54 +0000

    Fix for PR#3081474 to correct handling of isotopes in InChI output. I just needed to change isotab.GetExactMass() to etab.GetMass(). The problem was only exposed when converting molecules with isotopes of Br, where the most abundant isotope (79) is not the same as the atomic molecular weight (~80).

--- src/formats/inchiformat.cpp
+++ src/formats/inchiformat.cpp
@@ -330,7 +330,7 @@ bool InChIFormat::WriteMolecule(OBBase* pOb, OBConversion* pConv)
     if(patom->GetIsotope())
     {
       iat.isotopic_mass = ISOTOPIC_SHIFT_FLAG +
-        patom->GetIsotope() - (int)(isotab.GetExactMass(patom->GetAtomicNum())+0.5);
+        patom->GetIsotope() - (int)(etab.GetMass(patom->GetAtomicNum())+0.5);
     }
     else
       iat.isotopic_mass = 0 ;
