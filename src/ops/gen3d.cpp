e15b0b040b8eac33018e17a2a673624dc35ddc46
Noel O'Boyle <baoilleach@gmail.com>
2010-11-16 15:17:56 +0000

    Add XYZ test cases for chirality. No new failures here.

--- src/ops/gen3d.cpp
+++ src/ops/gen3d.cpp
@@ -21,6 +21,7 @@ GNU General Public License for more details.
 #include <openbabel/mol.h>
 #include <openbabel/builder.h>
 #include <openbabel/forcefield.h>
+#include <openbabel/obconversion.h>
 
 namespace OpenBabel
 {
@@ -33,10 +34,20 @@ public:
 
   virtual bool WorksWith(OBBase* pOb)const{ return dynamic_cast<OBMol*>(pOb)!=NULL; }
   virtual bool Do(OBBase* pOb, const char* OptionText=NULL, OpMap* pOptions=NULL, OBConversion* pConv=NULL);
+private:
+  void Debug(std::string prefix, OBMol* pmol);
 };
 
 /////////////////////////////////////////////////////////////////
 OpGen3D theOpGen3D("gen3D"); //Global instance
+
+void OpGen3D::Debug(std::string prefix, OBMol* pmol)
+{
+  /*std::cout << "Has stereo perceived " << pmol->HasChiralityPerceived() << "\n";
+  OBConversion conv;
+  conv.SetOutFormat("smi");
+  std::cout << prefix << " " << conv.WriteString(pmol) << "\n";*/
+}
 
 /////////////////////////////////////////////////////////////////
 bool OpGen3D::Do(OBBase* pOb, const char* OptionText, OpMap* pOptions, OBConversion* pConv)
@@ -44,9 +55,10 @@ bool OpGen3D::Do(OBBase* pOb, const char* OptionText, OpMap* pOptions, OBConvers
   OBMol* pmol = dynamic_cast<OBMol*>(pOb);
   if(!pmol)
     return false;
-
+  Debug("at start", pmol);
   OBBuilder builder;
   builder.Build(*pmol);
+  Debug("after build", pmol);
   pmol->SetDimension(3);
 
   OBForceField* pFF = OBForceField::FindForceField("MMFF94");
@@ -54,6 +66,12 @@ bool OpGen3D::Do(OBBase* pOb, const char* OptionText, OpMap* pOptions, OBConvers
     return true;
 
   pmol->AddHydrogens(false, true); // Add some hydrogens before running MMFF
+  Debug("after adding hydrogens", pmol);
+  pmol->DeleteHydrogens(); // Remove the hydrogens
+  Debug("after removing hydrogens", pmol);
+  pmol->AddHydrogens(false, true); // Add some hydrogens before running MMFF
+  Debug("after adding hydrogens", pmol);
+
   if (!pFF->Setup(*pmol)) {
     pFF = OBForceField::FindForceField("UFF");
     if (!pFF || !pFF->Setup(*pmol)) return true; // can't use either MMFF94 or UFF
@@ -61,7 +79,9 @@ bool OpGen3D::Do(OBBase* pOb, const char* OptionText, OpMap* pOptions, OBConvers
   pFF->SteepestDescent(250, 1.0e-4);
   pFF->WeightedRotorSearch(200, 25);
   pFF->ConjugateGradients(250, 1.0e-6);
+  Debug("after opt", pmol);
   pFF->UpdateCoordinates(*pmol);
+  Debug("after update coords", pmol);
 
   return true;
 }
