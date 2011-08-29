a347ef391f827f859a8c28d150bba30cf2bdf76e
Noel O'Boyle <baoilleach@gmail.com>
2011-08-29 14:30:34 +0000

    stereo/perception.cpp: (a) Rewrite TetStereoToWedgeHash() to use a scoring system to choose the best bond (simpler code). (b) Give a high score to bonds that are within DELTA_ANGLE_FOR_OVERLAPPING_BONDS of each other. This fixes some of the problems with Robert Kiss' test set (down from 146 failures to 120) as the InChI code regards such close bonds as ambiguous if using them to determine the stereo.

--- src/stereo/perception.cpp
+++ src/stereo/perception.cpp
@@ -39,6 +39,7 @@
 #define DEBUG 0
 #define DEBUG_INVERSIONS 0
 #define IMPLICIT_CIS_RING_SIZE 12
+#define DELTA_ANGLE_FOR_OVERLAPPING_BONDS 4.0 // In degrees
 
 using namespace std;
 
@@ -2577,38 +2578,61 @@ namespace OpenBabel {
         OBTetrahedralStereo *ts = dynamic_cast<OBTetrahedralStereo*>(*data);
         OBTetrahedralStereo::Config cfg = ts->GetConfig();
 
-        OBBond* chosen = (OBBond*) NULL;
+        // Find the two bonds closest in angle and remember them if
+        // they are closer than DELTA_ANGLE_FOR_OVERLAPPING_BONDS
         OBAtom* center = mol.GetAtomById(cfg.center);
-        bool nottet_flag = false;
-        bool acyclic_flag = false;
+        vector<OBAtom*> nbrs;
+        FOR_NBORS_OF_ATOM(a, center)
+          nbrs.push_back(&*a);
+        double min_angle = 359.0;
+        OBBond *close_bond_a, *close_bond_b;
+        for (unsigned int i=0; i<nbrs.size() - 1; ++i)
+          for (unsigned int j=i+1; j<nbrs.size(); ++j) {
+            double angle = abs(nbrs[i]->GetAngle(center, nbrs[j]));
+            if (angle < min_angle) {
+              min_angle = angle;
+              close_bond_a = mol.GetBond(center, nbrs[i]);
+              close_bond_b = mol.GetBond(center, nbrs[j]);
+            }
+          }
+        
+        if (min_angle > DELTA_ANGLE_FOR_OVERLAPPING_BONDS) {
+          close_bond_a = (OBBond*) NULL;
+          close_bond_b = (OBBond*) NULL;
+        }
+
         // Find the best candidate bond to set to up/down
         // 1. **Should not already be set**
         // 2. Should not be connected to a 2nd tet center
         // (this is acceptable, as the wedge is only at one end, but will only confuse things)
         // 3. Preferably is not in a cycle
         // 4. Preferably is a terminal H
+        // 5. If two bonds are overlapping, choose one of these
+        //    (otherwise the InChI code will mark it as ambiguous)
+
+        unsigned int max_bond_score = 0;
+        OBBond* chosen = (OBBond*) NULL;
         FOR_BONDS_OF_ATOM(b, center) {
-          if (alreadyset.find(&*b) == alreadyset.end()) {
-            if (chosen==NULL) chosen = &*b;
-            OBAtom* nbr = b->GetNbrAtom(center);
-            if (tetcenters.find(nbr->GetId()) == tetcenters.end()) { // Not a tetcenter
-              if (nottet_flag==false) {
-                chosen = &*b;
-                nottet_flag = true;
-              }
-              if (!b->IsInRing()) {
-                if (acyclic_flag==false) {
-                  chosen = &*b;
-                  acyclic_flag = true;
-                }
-                if (nbr->IsHydrogen()) {
-                  chosen = &*b;
-                  break;
-                }
-              }
-            }
+          if (alreadyset.find(&*b) != alreadyset.end()) continue;
+
+          OBAtom* nbr = b->GetNbrAtom(center);
+          unsigned int score = 1;
+
+          if (!b->IsInRing())
+            score += 2;
+          if (tetcenters.find(nbr->GetId()) == tetcenters.end()) // Not a tetcenter
+            score += 4;
+          if (nbr->IsHydrogen())
+            score += 8;
+          if (&*b==close_bond_a || &*b==close_bond_b)
+            score += 16;
+
+          if (score > max_bond_score) {
+            max_bond_score = score;
+            chosen = &*b;
           }
         }
+
         if (chosen==NULL) { // There is a remote possibility of this but let's worry about 99.9% of cases first
           obErrorLog.ThrowError(__FUNCTION__,
             "Failed to set stereochemistry as unable to find an available bond", obError);
