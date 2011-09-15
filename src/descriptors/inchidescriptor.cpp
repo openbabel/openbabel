bf88e6efee4c02f2ac275dd7c781dd73666d69dc
Chris Morley <c.morley@gaseq.co.uk>
2011-09-15 20:25:38 +0000

    Add a descriptor "inchikey"

--- src/descriptors/inchidescriptor.cpp
+++ src/descriptors/inchidescriptor.cpp
@@ -30,27 +30,28 @@ namespace OpenBabel
 class InChIFilter : public OBDescriptor
 {
 public:
-  InChIFilter(const char* ID) : OBDescriptor(ID){};
-  virtual const char* Description(){return "IUPAC InChI identifier";};
+  InChIFilter(const char* ID, bool useKey=false) : OBDescriptor(ID), bKey(useKey) {};
+  virtual const char* Description()
+  {
+    return bKey ? "InChIKey" : "IUPAC InChI identifier";
+  }
   virtual bool Compare(OBBase* pOb, istream& optionText, bool noEval, std::string* param=NULL);
   virtual double GetStringValue(OBBase* pOb, std::string& svalue, std::string* param=NULL);
-//  virtual bool LessThan(OBBase* pOb1, OBBase* pOb2);
-//  virtual void Init(){cache.clear();}
-//  void GetCachedValue(OBBase* pOb, string& s);
   virtual bool Order(std::string s1, std::string s2)
   {
     InChIFormat::InchiLess f;
     return f(s1, s2);
   }
 private:
-  map<OBBase*, string> cache;
+  bool bKey;
 };
 
 double InChIFilter::GetStringValue(OBBase* pOb, std::string& svalue, std::string*)
 {
   OBConversion conv;
   conv.AddOption("w");//suppress trivial warnings
-
+  if(bKey)
+    conv.AddOption("K");
   if(conv.SetOutFormat("inchi"))
     svalue = conv.WriteString(pOb);
   else
@@ -64,58 +65,40 @@ bool InChIFilter::Compare(OBBase* pOb, istream& optionText, bool noEval, std::st
 {
   string InchiFilterString, inchi;
   string::size_type filterpos=0, inchipos, len;
+  bool ret;
   bool matchornegate = ReadStringFromFilter(optionText, InchiFilterString);
   if(noEval)
     return false;
   GetStringValue(pOb, inchi);
-  inchipos = inchi.find('/');
+  if(!bKey)
+  {
+    inchipos = inchi.find('/');
 
-  //See if filterstring starts with "InChI=1/"
-  if(InchiFilterString.find(inchi.substr(0,inchipos))==0)
-    filterpos=inchipos+1;
-  //If filterstring starts with a number, set filterpos after the next '/'(for pasted InChIs)
-  if(isdigit(InchiFilterString[0]))
-    filterpos=InchiFilterString.find('/')+1;
+    //See if filterstring starts with "InChI=1/"
+    if(InchiFilterString.find(inchi.substr(0,inchipos))==0)
+      filterpos=inchipos+1;
+    //If filterstring starts with a number, set filterpos after the next '/'(for pasted InChIs)
+    if(isdigit(InchiFilterString[0]))
+      filterpos=InchiFilterString.find('/')+1;
 
-  //Considering only the significant parts,
-  //compare InChI and filter string, only to length of filter string
-  len = InchiFilterString.size() - filterpos;
-  bool ret = inchi.compare(inchipos+1, len, InchiFilterString, filterpos, len)==0;
+    //Considering only the significant parts,
+    //compare InChI and filter string, only to length of filter string
+    len = InchiFilterString.size() - filterpos;
+    ret = inchi.compare(inchipos+1, len, InchiFilterString, filterpos, len)==0;
+  }
+  else
+    ret = (inchi==InchiFilterString);
 
   if(!matchornegate)
     ret = !ret;
   return ret;
 }
 
-/*
-///Compare InChI strings in a chemically sensible way
-// See InChIFormat::InchiLess()
-bool InChIFilter::LessThan(OBBase* pOb1, OBBase* pOb2)
-{
-  string s1, s2;
-  //There is a cache, indexed by the molecule pointer, to reduce
-  //the number of InChI evaluations. This is potentially large when sorting.
-  GetCachedValue(pOb1, s1);
-  GetCachedValue(pOb2, s2);
-  InChIFormat::InchiLess f;
-  return f(s1, s2);
-}
-void InChIFilter::GetCachedValue(OBBase* pOb, string& s)
-{
-  map<OBBase*, string>::iterator itr;
-  itr = cache.find(pOb);
-  if(itr!=cache.end())
-    s = itr->second;
-  else
-  {
-    GetStringValue(pOb, s);
-    cache[pOb] =s;
-  }
-}
-*/
 //**********************************************
-//Make a global instance
+//Make global instances
 InChIFilter theInChIFilter("InChI");
+InChIFilter keyInChIFilter("InChIKey",true);
+//**********************************************
 
 }//namespace
 
