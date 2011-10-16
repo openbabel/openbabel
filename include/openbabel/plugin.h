cd64d85b9d176ca12146a6147d7a55a0fbb2946b
Chris Morley <c.morley@gaseq.co.uk>
2011-10-16 19:23:08 +0000

    Apply patch 3424378 by My Th formating the macro code in plugin.h

--- include/openbabel/plugin.h
+++ include/openbabel/plugin.h
@@ -148,36 +148,71 @@ protected:
 //Macro to be added to definition of the base class
 #define MAKE_PLUGIN(BaseClass)\
 protected:\
-static PluginMapType& Map();\
-virtual PluginMapType& GetMap()const{return Map();}\
+  static PluginMapType& Map();\
+  virtual PluginMapType& GetMap() const {\
+    return Map();\
+  }\
 public:\
-static BaseClass*& Default(){static BaseClass* d;return d;}\
-  BaseClass(const char* ID, bool IsDefault=false)\
- {_id=ID;if(ID&&*ID){if(IsDefault || Map().empty()) Default() = this;\
- Map()[ID]=this;PluginMap()[TypeID()] =this;}}\
-static BaseClass* FindType(const char* ID)\
- {if(!ID || *ID==0 || *ID==' ') return Default();\
- return static_cast<BaseClass*>(BaseFindType(Map(),ID));}
+  static BaseClass*& Default() {\
+    static BaseClass* d;\
+    return d;\
+  }\
+  BaseClass(const char* ID, bool IsDefault=false) {\
+    _id=ID;\
+    if (ID&&*ID) {\
+      if (IsDefault || Map().empty()) {\
+        Default() = this;\
+      }\
+      Map()[ID] = this;\
+      PluginMap()[TypeID()] = this;\
+    }\
+  }\
+  static BaseClass* FindType(const char* ID) {\
+    if (!ID || *ID==0 || *ID==' ') {\
+      return Default();\
+    }\
+    return static_cast<BaseClass*>(BaseFindType(Map(),ID));\
+  }
 
 #define PLUGIN_CPP_FILE(BaseClass)\
-OBPlugin::PluginMapType& BaseClass::Map()\
-{ static OBPlugin::PluginMapType map; return map; }
+OBPlugin::PluginMapType& BaseClass::Map() {\
+  static OBPlugin::PluginMapType map;\
+  return map;\
+}
 
 #else // __CYGWIN__ || __MINGW32__
 
 //Macro to be added to definition of the base class
 #define MAKE_PLUGIN(BaseClass)\
 protected:\
-static PluginMapType& Map(){static PluginMapType m;return m;}\
-virtual PluginMapType& GetMap()const{return Map();}\
+  static PluginMapType& Map() {\
+    static PluginMapType m;\
+    return m;\
+  }\
+  virtual PluginMapType& GetMap() const {\
+    return Map();\
+  }\
 public:\
-static BaseClass*& Default(){static BaseClass* d;return d;}\
-  BaseClass(const char* ID, bool IsDefault=false)\
- {_id=ID;if(ID&&*ID){if(IsDefault || Map().empty()) Default() = this;\
- Map()[ID]=this;PluginMap()[TypeID()] =this;}}\
-static BaseClass* FindType(const char* ID)\
- {if(!ID || *ID==0 || *ID==' ') return Default();\
- return static_cast<BaseClass*>(BaseFindType(Map(),ID));}
+  static BaseClass*& Default() {\
+    static BaseClass* d;\
+    return d;\
+  }\
+  BaseClass(const char* ID, bool IsDefault=false) {\
+    _id=ID;\
+    if (ID&&*ID) {\
+      if (IsDefault || Map().empty()) {\
+        Default() = this;\
+      }\
+      Map()[ID] = this;\
+      PluginMap()[TypeID()] = this;\
+    }\
+  }\
+  static BaseClass* FindType(const char* ID) {\
+    if (!ID || *ID==0 || *ID==' ') {\
+      return Default();\
+    }\
+    return static_cast<BaseClass*>(BaseFindType(Map(),ID));\
+  }
 
 #endif // __CYGWIN__ || __MINGW32__
 
