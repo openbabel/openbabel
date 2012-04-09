aff9b3a8c2153b4aedecd8f79828d6b389dcabab
Noel O'Boyle <baoilleach@gmail.com>
2012-04-09 18:37:45 +0000

    commandpainter.h, commandpainter.cpp: A new painter (not added to public API) that simply reports the various API calls as text. painterformat.cpp: A new output format that reports the commands used to create a 2D depiction.

---
+++ include/openbabel/depict/commandpainter.h
@@ -0,0 +1,57 @@
+/**********************************************************************
+commandpainter.h  - Render as depiction commands
+
+Copyright (C) 2012 by Noel O'Boyle
+
+This file is part of the Open Babel project.
+For more information, see <http://openbabel.org/>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+***********************************************************************/
+#ifndef OB_COMMANDPAINTER_H
+#define OB_COMMANDPAINTER_H
+
+#include <openbabel/depict/painter.h>
+
+namespace OpenBabel
+{
+  class CommandPainter : public OBPainter
+  {
+    public:
+      CommandPainter(std::ostream& ofs);
+      ~CommandPainter();
+      //! @name OBPainter methods
+      //@{
+      void NewCanvas(double width, double height);
+      bool IsGood() const;
+      void SetFontFamily(const std::string &fontFamily) {} // FIXME
+      void SetFontSize(int pointSize);
+      void SetFillColor(const OBColor &color);
+      void SetPenColor(const OBColor &color);
+      void SetPenWidth(double width);
+      void DrawLine(double x1, double y1, double x2, double y2);
+      void DrawPolygon(const std::vector<std::pair<double,double> > &points);
+      void DrawCircle(double x, double y, double r);
+      void DrawText(double x, double y, const std::string &text);
+      OBFontMetrics GetFontMetrics(const std::string &text);
+      //@}
+
+      //! @name CommandPainter specific
+      //@{
+      //@}
+ 
+    private:      
+      std::ostream& m_ofs;
+      std::streamsize m_oldprec;
+  };
+
+}
+
+#endif
---
+++ src/depict/commandpainter.cpp
@@ -0,0 +1,110 @@
+/**********************************************************************
+commandpainter.cpp  - Render as depiction commands
+
+Copyright (C) 2012 by Noel O'Boyle
+
+This file is part of the Open Babel project.
+For more information, see <http://openbabel.org/>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+***********************************************************************/
+#include <openbabel/obutil.h>
+#include <openbabel/depict/commandpainter.h>
+
+#include <iostream>
+using namespace std;
+
+namespace OpenBabel
+{
+
+  // Class definition of CommandPainter
+  CommandPainter::CommandPainter(ostream& ofs): m_ofs(ofs)
+  {
+    m_oldprec = m_ofs.precision(1);
+    m_ofs << fixed;
+  }
+
+  CommandPainter::~CommandPainter()
+  {
+    m_ofs.precision(m_oldprec);
+    m_ofs.unsetf(ios_base::fixed);
+  }
+
+  void CommandPainter::NewCanvas(double width, double height)
+  {
+    m_ofs << "NewCanvas " << width << " " << height << endl;
+  }
+  
+  bool CommandPainter::IsGood() const
+  {
+    return true;
+  }
+      
+  void CommandPainter::SetFontSize(int pointSize)
+  {
+    m_ofs << "SetFontSize " << pointSize << endl;
+  }
+
+  void CommandPainter::SetFillColor(const OBColor &color)
+  {
+    m_ofs << "SetFillColor " << color.red << " " << color.green << " " << color.blue << " " << color.alpha << " (rgba)" << endl;
+  }
+
+  void CommandPainter::SetPenColor(const OBColor &color)
+  {
+    m_ofs << "SetPenColor " << color.red << " " << color.green << " " << color.blue << " " << color.alpha << " (rgba)" << endl;
+  }
+      
+  void CommandPainter::SetPenWidth(double width)
+  {
+    m_ofs << "SetPenWidth " << width << endl;
+  }
+
+  void CommandPainter::DrawLine(double x1, double y1, double x2, double y2)
+  {
+    
+    m_ofs << fixed << "DrawLine " << x1 << " " << y1 << " to "
+                                  << x2 << " " << y2 << endl;
+  }
+
+  void CommandPainter::DrawPolygon(const std::vector<std::pair<double,double> > &points)
+  {
+    m_ofs << "DrawPolygon ";
+    std::vector<std::pair<double,double> >::const_iterator i;
+    for (i = points.begin(); i != points.end(); ++i) {
+      if (i != points.begin())
+        m_ofs << " to ";
+      m_ofs << i->first << " " << i->second;
+    }
+    m_ofs << endl;
+  }
+
+  void CommandPainter::DrawCircle(double x, double y, double r)
+  {
+    m_ofs << "DrawCircle " << x << " " << y << " radius " << r << endl;
+  }
+
+  void CommandPainter::DrawText(double x, double y, const std::string &text)
+  {
+    m_ofs << "DrawText " << x << " " << y << " \"" << text << "\"" << endl;
+  }
+
+  OBFontMetrics CommandPainter::GetFontMetrics(const std::string &text)
+  {
+    OBFontMetrics metrics;
+    metrics.fontSize = 0;
+    metrics.ascent = 0;
+    metrics.descent = 0;
+    metrics.width = 0;
+    metrics.height = 0;
+    return metrics;
+  }
+}
+
