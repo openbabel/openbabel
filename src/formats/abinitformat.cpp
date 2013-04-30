361a2a7ebbb5bc8acc4ecc0b9288f7bcfc41b411
Ronald Cohen <cohen@gl.ciw.edu>
2013-04-30 13:14:47 -0400

    Fixed so that works with multiple line typat , correctly reads lattice and atomic positions with abinit 6.15.2

--- src/formats/abinitformat.cpp
+++ src/formats/abinitformat.cpp
@@ -79,7 +79,7 @@ namespace OpenBabel
     vector<string> vs;
 
     OBAtom *atom;
-    int natom=-1;
+    int natom;
     vector<int> atomicNumbers, atomTypes;
     double x, y, z;
     vector<vector3> atomPositions;
@@ -91,11 +91,18 @@ namespace OpenBabel
     double acell[3]; // scale of lattice vectors
     int numTranslationVectors = 0;
     int symmetryCode = 0;
-
+    bool last = false;
     mol.BeginModify();
 
     while (ifs.getline(buffer,BUFF_SIZE))
       {
+	if(strstr(buffer,"outvars")){
+	     last = true;
+	     continue;
+	   }
+	if(! last){
+	  continue;
+	}
         // tokens are listed in alphabetical order for code clarity
         if (strstr(buffer, "acell")) {
           tokenize(vs, buffer);
@@ -107,33 +114,33 @@ namespace OpenBabel
             acell[i] = atof(vs[i+1].c_str());
           }
         }
-        // Sometimes Cartesian has lower-case letter
-        else if 
-          ((strstr(buffer, "artesian coordinates")) &&
-           (strstr(buffer, "(xcart)")) &&
-           (natom > -1) //be sure natom has already been read
-           )
-          {
+        else if (strstr(buffer, " xcart ")) {
           double unit = BOHR_TO_ANGSTROM;
           if (strstr(buffer, "ngstrom"))
             unit = 1.0; // no conversion needed
-          for (int i=0;i<natom;i++)
-            {
-              ifs.getline(buffer,BUFF_SIZE);
-              tokenize(vs, buffer);
-              if (vs.size() != 3) {
-                obErrorLog.ThrowError
-                  (__FUNCTION__,
-                   "error while parsing coordinates of atoms", obWarning);
-                return(false);
-              }
-              x = atof(vs[0].c_str()) * unit;
-              y = atof(vs[1].c_str()) * unit;
-              z = atof(vs[2].c_str()) * unit;
-              atomPositions.push_back(vector3(x, y, z));
-              // get next line
-            }
+
+	  //          ifs.getline(buffer,BUFF_SIZE);
+          tokenize(vs, buffer);
+
+          // first line, rprim takes up a token
+          x = atof((char*)vs[1].c_str()) * unit;
+          y = atof((char*)vs[2].c_str()) * unit;
+	  z = atof((char*)vs[3].c_str()) * unit;
+	  atomPositions.push_back(vector3(x, y, z));
+	  // get next line
+	  ifs.getline(buffer,BUFF_SIZE);
+	  tokenize(vs, buffer);
+	  //rest of lines
+	  while(vs.size() == 3) {
+	    x = atof(vs[0].c_str()) * unit;
+	    y = atof(vs[1].c_str()) * unit;
+	    z = atof(vs[2].c_str()) * unit;
+            atomPositions.push_back(vector3(x, y, z));
+            // get next line
+            ifs.getline(buffer,BUFF_SIZE);
+            tokenize(vs, buffer);
           }
+      }
         else if (strstr(buffer, "natom")) {
           tokenize(vs, buffer);
           if (vs.size() != 2)
@@ -143,15 +150,16 @@ namespace OpenBabel
         else if (strstr(buffer, "rprim")) {
           numTranslationVectors = 0;
           int column;
-          for (int i = 0; i < 3; ++i) {
+	  ifs.getline(buffer,BUFF_SIZE);
+          for (int i = 1; i < 4; ++i) {
             tokenize(vs, buffer);
             if (vs.size() < 3)
               break;
 
             // first line, rprim takes up a token
-            if (i == 0)
-              column = 1;
-            else
+	    //            if (i == 0)
+	    // column = 1;
+            //else
               column = 0;
 
             x = atof((char*)vs[column].c_str()) * BOHR_TO_ANGSTROM;
@@ -166,12 +174,22 @@ namespace OpenBabel
           // Should be something like (#160)
           symmetryCode = atoi(vs[1].substr(1).c_str());
         }
-        else if (strstr(buffer, "typat")) {
-          tokenize(vs, buffer);
+        else if (strstr(buffer, " typat ")) {
           atomTypes.clear();
-          for (unsigned int i = 1; i < vs.size(); ++i) {
-            atomTypes.push_back(atoi(vs[i].c_str()));
-          }
+	  tokenize(vs, buffer);
+	  for (unsigned int i = 1; i < vs.size(); ++i) {
+	    atomTypes.push_back(atoi(vs[i].c_str()));
+	  }
+	  ifs.getline(buffer,BUFF_SIZE);
+	  int n=0;
+	  while( n+vs.size()<natom){
+	    tokenize(vs, buffer);
+	    for (unsigned int i = 0; i < vs.size(); ++i) {
+	      atomTypes.push_back(atoi(vs[i].c_str()));
+	    }
+	    n+=vs.size();
+	    ifs.getline(buffer,BUFF_SIZE);
+	  }
         }
         else if (strstr(buffer, "znucl")) {
           tokenize(vs, buffer);
@@ -223,7 +241,8 @@ namespace OpenBabel
     // Attach unit cell translation vectors if found
     if (numTranslationVectors > 0) {
       OBUnitCell* uc = new OBUnitCell;
-      uc->SetData(acell[0] * translationVectors[0], acell[1] * translationVectors[1], acell[2] * translationVectors[2]);
+	    //      uc->SetData(acell[0] * translationVectors[0], acell[1] * translationVectors[1], acell[2] * translationVectors[2]);
+      uc->SetData(translationVectors[0], translationVectors[1], translationVectors[2]);
       uc->SetOrigin(fileformatInput);
       if (symmetryCode)
         uc->SetSpaceGroup(symmetryCode);
