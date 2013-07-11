54ddefbdeb0f5731e5c3a9a9efc4625b25917ee8
Noel O'Boyle <baoilleach@gmail.com>
2013-07-11 21:37:50 +0100

    Final changes to make confab ready for merging: 1. Revert changes to IsSetupNeed() as asked by Geoff 2. Remove calcrmsd and confab executables

--- src/forcefield.cpp
+++ src/forcefield.cpp
@@ -978,7 +978,6 @@ namespace OpenBabel
   // avogadro's AutoOpt tool
   bool OBForceField::IsSetupNeeded(OBMol &mol)
   {
-    /*
     if (_mol.NumAtoms() != mol.NumAtoms())
       return true;
 
@@ -1003,9 +1002,9 @@ namespace OpenBabel
           || bond->GetEndAtom()->GetAtomicNum()
           != (mol.GetBond(bond->GetIdx()))->GetEndAtom()->GetAtomicNum())
         return true;
-    }*/
+    }
 
-    return true; // Force setup always - this test fails incorrectly sometimes
+    return false;
   }
 
   bool OBForceField::GetAtomTypes(OBMol &mol)
