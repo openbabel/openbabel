180e5ef56f5bc5ec31031351826cd02e84988669
Noel O'Boyle <baoilleach@gmail.com>
2013-08-31 10:02:29 +0100

    Replace ref to shared_ptr to obsharedptr

--- src/alias.cpp
+++ src/alias.cpp
@@ -21,6 +21,7 @@ GNU General Public License for more details.
 #include <openbabel/parsmart.h>
 #include <openbabel/mcdlutil.h>
 #include <openbabel/atomclass.h>
+#include <openbabel/shared_ptr.h>
 
 using namespace std;
 namespace OpenBabel
@@ -270,7 +271,7 @@ bool AliasData::LoadFile(SmartsTable& smtable)
         //OBSmartsPattern objects are not copyable without complications,
         //so reference semantics used.
 
-        shared_ptr<OBSmartsPattern> psp(new OBSmartsPattern);
+        obsharedptr<OBSmartsPattern> psp(new OBSmartsPattern);
         psp->Init(ssmarts.str());
         smtable.push_back(make_pair(vec[0], psp));
       }
