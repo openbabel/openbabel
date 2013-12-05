9024b6b05d5d769ad36c4722994be183a97aa25f
Anders Steen Christensen <andersx@nano.ku.dk>
2013-12-05 16:15:32 +0100

    Added functions for cross product and dot product

--- src/mol.cpp
+++ src/mol.cpp
@@ -212,6 +212,111 @@ namespace OpenBabel
                             ((OBAtom*)_vatom[d-1])->GetVector()));
   }
 
+
+    double OBMol::np_cross(double a1, double a2, double a3,
+                           double b1, double b2, double b3) {
+
+        double cross[3];
+
+        cross[0] = a2 * b3
+                 - b2 * a3;
+
+        cross[1] = a3 * b1
+                 - b3 * a1;
+
+        cross[2] = a1 * b2
+                 - b1 * a2;
+
+        return cross;
+    }
+
+
+
+    double OBMol::np_dot(double v1x, double v1y, double v1z,
+                         double v2x, double v2y, double v2z) {
+
+        double dot = v1x * v2x
+                   + v1y + v2y
+                   + v1z * v2z;
+
+        return dot;
+    }
+
+    double OBMol::CalcTorsionForSet(vector<int> tor) {
+
+        // b1 = b - a
+        // b2 = c - b
+        // b3 = d - c
+        // rb2 = np.sqrt(np.dot(b2,b2))
+        // b2xb3 = np.cross(b2,b3)
+        // b1xb2 = np.cross(b1,b2)
+        // dihedral = math.atan2( np.dot(rb2*b1,b2xb3), np.dot(b1xb2,b2xb3) )
+        // return dihedral
+
+        double v1x,v1y,v1z,v2x,v2y,v2z,v3x,v3y,v3z;
+        double c1x,c1y,c1z,c2x,c2y,c2z,c3x,c3y,c3z;
+        double c1mag,c2mag,radang,costheta,m[9];
+        double x,y,z,mag,rotang,sn,cs,t,tx,ty,tz;
+
+        //calculate the torsion angle
+
+        v1x = _c[tor[0]]   - _c[tor[1]];
+        v1y = _c[tor[0]+1] - _c[tor[1]+1];
+        v1z = _c[tor[0]+2] - _c[tor[1]+2];
+
+        v2x = _c[tor[1]]   - _c[tor[2]];
+        v2y = _c[tor[1]+1] - _c[tor[2]+1];
+        v2z = _c[tor[1]+2] - _c[tor[2]+2];
+
+        v3x = _c[tor[2]]   - _c[tor[3]];
+        v3y = _c[tor[2]+1] - _c[tor[3]+1];
+        v3z = _c[tor[2]+2] - _c[tor[3]+2];
+
+        // v1 ~ b1
+        // v2 ~ b2
+        // v3 ~ b3
+
+        rv2 = math.sqrt( np_dot(v2x, v2y, v2z,
+                                v2x, v2y, v2z));
+
+        // rv2 ~ rb2
+
+        c1x = v1y*v2z - v1z*v2y;
+        c2x = v2y*v3z - v2z*v3y;
+        c3x = c1y*c2z - c1z*c2y;
+
+        c1y = -v1x*v2z + v1z*v2x;
+        c2y = -v2x*v3z + v2z*v3x;
+        c3y = -c1x*c2z + c1z*c2x;
+
+        c1z = v1x*v2y - v1y*v2x;
+        c2z = v2x*v3y - v2y*v3x;
+        c3z = c1x*c2y - c1y*c2x;
+
+        c1mag = SQUARE(c1x)+SQUARE(c1y)+SQUARE(c1z);
+        c2mag = SQUARE(c2x)+SQUARE(c2y)+SQUARE(c2z);
+        if (c1mag*c2mag < 0.01)
+          costheta = 1.0; //avoid div by zero error
+        else
+          costheta = (c1x*c2x + c1y*c2y + c1z*c2z)/(sqrt(c1mag*c2mag));
+
+        if (costheta < -0.999999)
+          costheta = -0.999999;
+        if (costheta >  0.999999)
+          costheta =  0.999999;
+
+        if ((v2x*c3x + v2y*c3y + v2z*c3z) > 0.0)
+          radang = -acos(costheta);
+        else
+          radang = acos(costheta);
+
+
+        return radang
+
+    }
+
+
+
   void OBMol::SetTorsion(OBAtom *a,OBAtom *b,OBAtom *c, OBAtom *d, double ang)
   {
     vector<int> tor;
