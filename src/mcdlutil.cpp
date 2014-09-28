cefe0d3b513c41b2e468ea565c65be34767cc8b6
Lee-Ping <leeping@stanford.edu>
2014-09-28 13:24:25 -0700

    Add a threshold to single fragment scaling

--- src/mcdlutil.cpp
+++ src/mcdlutil.cpp
@@ -5403,7 +5403,11 @@ namespace OpenBabel {
       if ((sm->getAtom(n)->ry < yMin) || (yMin == RUNDEF)) yMin=sm->getAtom(n)->ry;
       if ((sm->getAtom(n)->ry > yMax) || (yMax == RUNDEF)) yMax=sm->getAtom(n)->ry;
     };
-    if ((xMax == xMin) && (yMax == yMin)) {
+    // LPW: Nearly horizontal molecules were being scaled which resulted in
+    // some nonsensical structures.  This code ensures that coordinates with 
+    bool Xeq = (abs(xMax - xMin) < 0.01);
+    bool Yeq = (abs(yMax - yMin) < 0.01);
+    if ((Xeq) && (Yeq)) {
       for (i=0; i<atomList->size(); i++) {
         n=(*atomList)[i];
         sm->getAtom(n)->rx=(pf.fragLeft+pf.fragWidth/2);
@@ -5411,9 +5415,9 @@ namespace OpenBabel {
       };
       return;
     };
-    if (xMin == xMax) {         //y should be scaled
+    if (Xeq) {         //y should be scaled
       scale=(pf.fragHeight-2*offset)/(yMax-yMin);
-    } else if (yMin == yMax) { //x should be scaled
+    } else if (Yeq) { //x should be scaled
       scale=(pf.fragWidth-2*offset)/(xMax-xMin);
     } else {
       r1=(pf.fragHeight-2*offset)/(yMax-yMin);
