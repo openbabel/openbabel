c247d5d85a84288bcd27ba779c6cd4d873534557
M J Harvey <matt@mjharvey.org.uk>
2015-04-29 17:08:44 +0200

    Pointcloud format for producing points quasi-randomly distributed on a VDW surface. Added optional probe radius, changed defaout output to bare {x,y,z} tuple with new option "-xx" for outputing in more readily-consumed XYZ format

--- include/openbabel/plugin.h
+++ include/openbabel/plugin.h
@@ -461,6 +461,7 @@ public:
 #ifdef HAVE_LIBZ
   OB_STATIC_PLUGIN(PNGFormat, thePNGFormat)
 #endif
+  OB_STATIC_PLUGIN(PointCloudFormat, thePointCloudFormat)
   OB_STATIC_PLUGIN(PovrayFormat, thePovrayFormat)
   OB_STATIC_PLUGIN(PQRFormat, thePQRFormat)
   OB_STATIC_PLUGIN(PQSFormat, thePQSFormat)
