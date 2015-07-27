dec5a580bf3189ff0aeb83c5c3c617ff1626deb1
David Koes <dkoes@pitt.edu>
2015-07-27 17:15:20 -0400

    don't reset conformer in addh

--- src/mol.cpp
+++ src/mol.cpp
@@ -2282,7 +2282,6 @@ namespace OpenBabel
       }
 
     DecrementMod();
-    SetConformer(0);
 
     //reset atom type and partial charge flags
     _flags &= (~(OB_PCHARGE_MOL|OB_ATOMTYPES_MOL|OB_SSSR_MOL|OB_AROMATIC_MOL));
