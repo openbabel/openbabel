4a7ae31ed9247c60a2ed8edb6181e9776c0b4bdb
xomachine <xomachiner@gmail.com>
2015-07-28 16:06:16 +0300

    Major restructurization of nwchem format reading. New structure allows recognition of sequential calculations like "optimize->freq->property". Optimization steps support added.

--- src/formats/nwchemformat.cpp
+++ src/formats/nwchemformat.cpp
@@ -40,6 +40,10 @@ namespace OpenBabel
         "NWChem output format\n"
         "Read Options e.g. -as\n"
         " s  Output single bonds only\n"
+        " f  Overwrite molecule if more than one\n"
+        "calculations with different molecules\n"
+        "input in one output file detected\n"
+        "(last calculation will be prefered)\n"
         " b  Disable bonding entirely\n\n";
     };
 
@@ -59,7 +63,8 @@ namespace OpenBabel
 
   private:
     OBMol* ReadCoordinates(istream *ifs);
-    OBVibrationData* ReadFrequencies(istream* ifs, OBVibrationData* vibration_data);
+    OBVibrationData* ReadFrequencyCalculation(istream* ifs);
+    OBMol* ReadGeometryOptimizationCalculation(istream* ifs);
 
     enum CalculationType
     {
@@ -148,100 +153,184 @@ namespace OpenBabel
           break;
         tokenize(vs,buffer);
       }
-
     atoms->EndModify();
-
     return atoms;
   }
 
   //////////////////////////////////////////////////////
-  /*
-  Method reads vibration data from input stream (ifs) and writes it to existent
-  OBVibrationData object or creates new OBVibrationData object when it does
-  not exist.
-  Input stream must be set to begining of frequency table in nwo file. (Line after
-  "(Projected Frequencies expressed in cm-1)")
-  Method returns true when vibration data read and written to OBVibrationData successful
-  or returns false when supplied OBVibrationData object contain frequencies not related to
-  read ones.
+  /**
+  Method reads optimization steps from input stream (ifs)
+  and writes it to new OBMol object, then returns it.
+  Input stream must be set to begining of geometry optimization
+  calculation in nwo file. (Line after "NWChem Geometry Optimization")
+  If no geometry data found then NULL will be returned.
   */
-  OBVibrationData* NWChemOutputFormat::ReadFrequencies(istream* ifs, OBVibrationData* vibration_data)
+  OBMol* NWChemOutputFormat::ReadGeometryOptimizationCalculation(istream* ifs)
   {
+    vector<double> energies;
+    vector<string> vs;
+    char buffer[BUFF_SIZE];
 
-    vector<double>  Frequencies;
+    OBMol *pmol = NULL;
+
+    while (ifs->getline(buffer, BUFF_SIZE) != NULL)
+    {
+        if(strstr(buffer,"Output coordinates") != NULL)
+        {
+            OBMol* geometry = ReadCoordinates(ifs);
+            unsigned int natoms = geometry->NumAtoms();
+            double *conformer = new double[3*natoms];
+            double *coordinates = geometry->GetCoordinates();
+            memcpy(conformer, coordinates, sizeof(double)*3*natoms);
+
+            if (pmol == NULL)
+            {
+                pmol = geometry;
+                delete [] conformer;
+            }
+            else
+            {
+                //for(unsigned int i = 0;i < natoms;i++)
+                //    pmol->GetAtomById(i)->SetVector(geometry->GetAtomById(i)->GetVector());
+                delete geometry;
+                pmol->AddConformer(conformer);
+            }
+        }
+        else if(strstr(buffer,"Step       Energy") != NULL)
+        {
+            // Extract energy
+            ifs->getline(buffer, BUFF_SIZE); // ------
+            ifs->getline(buffer, BUFF_SIZE);
+            tokenize(vs, buffer);
+            energies.push_back(atof(vs[2].c_str()));
+        }
+        else if(strstr(buffer, "Task  times  cpu") != NULL)
+        {
+            // End of task
+            break;
+        } // if "Task  times  cpu"
+    }
+    for(unsigned int i = 0;i < pmol->NumAtoms();i++)
+    {
+        double *geometry = pmol->GetConformer(pmol->NumConformers() - 1);
+        pmol->GetAtomById(i)->SetVector(geometry[i*3+0],
+                                        geometry[i*3+1],
+                                        geometry[i*3+2]);
+    }
+    pmol->SetEnergies(energies);
+    return pmol;
+  }
+
+  //////////////////////////////////////////////////////
+  /**
+  Method reads vibration data from input stream (ifs)
+  and writes it to new OBVibrationData object, then
+  returns it.
+  Input stream must be set to begining of frequency
+  calculation in nwo file. (Line after "NWChem <theory> Module")
+  If vibration data not found then NULL will be returned.
+  */
+  OBVibrationData* NWChemOutputFormat::ReadFrequencyCalculation(istream* ifs)
+  {
+    OBVibrationData* vibration_data = NULL;
+    vector<double>  Frequencies, Intensities;
     vector<vector<vector3> > Lx;
     vector<string> vs;
     char buffer[BUFF_SIZE];
 
-    ifs->getline(buffer, BUFF_SIZE); // blank line
-    ifs->getline(buffer, BUFF_SIZE); // frequency counting
-    ifs->getline(buffer, BUFF_SIZE); // blank line
-    ifs->getline(buffer, BUFF_SIZE); // P.Frequency  etc.
-    while (strstr(buffer,"P.Frequency") != NULL)
-      {
-        vector<double> freq;
-        vector<vector<vector3> > vib;
-        // freq and vib are auxiliary vectors which hold the data for
-        // every block of 6 vibrations.
-        tokenize(vs,buffer);
-        for(unsigned int i=1; i<vs.size(); ++i)
-            freq.push_back(atof(vs[i].c_str()));
-        ifs->getline(buffer,BUFF_SIZE);     // blank line
-        ifs->getline(buffer,BUFF_SIZE);
-        tokenize(vs,buffer);
-        while(vs.size() > 2) {
-          vector<double> x, y, z;
-          for (unsigned int i = 1; i < vs.size(); i++)
-            x.push_back(atof(vs[i].c_str()));
-          ifs->getline(buffer, BUFF_SIZE);
-          tokenize(vs,buffer);
-          for (unsigned int i = 1; i < vs.size(); i++)
-            y.push_back(atof(vs[i].c_str()));
-          ifs->getline(buffer, BUFF_SIZE);
-          tokenize(vs,buffer);
-          for (unsigned int i = 1; i < vs.size(); i++)
-            z.push_back(atof(vs[i].c_str()));
-          for (unsigned int i = 0; i < freq.size(); i++) {
-            vib.push_back(vector<vector3>());
-            vib[i].push_back(vector3(x[i], y[i], z[i]));
-          }
-          ifs->getline(buffer, BUFF_SIZE);
-          tokenize(vs,buffer);
-        } // while
+    while (ifs->getline(buffer, BUFF_SIZE) != NULL)
+    {
+        if (strstr(buffer,"P.Frequency") != NULL)
+        {
+            vector<double> freq;
+            vector<vector<vector3> > vib;
+            // freq and vib are auxiliary vectors which hold the data for
+            // every block of 6 vibrations.
+            tokenize(vs,buffer);
+            for(unsigned int i=1; i<vs.size(); ++i)
+                freq.push_back(atof(vs[i].c_str()));
+            ifs->getline(buffer,BUFF_SIZE);     // blank line
+            ifs->getline(buffer,BUFF_SIZE);
+            tokenize(vs,buffer);
+            while(vs.size() > 2)
+            {
+              vector<double> x, y, z;
+              for (unsigned int i = 1; i < vs.size(); i++)
+                x.push_back(atof(vs[i].c_str()));
+              ifs->getline(buffer, BUFF_SIZE);
+              tokenize(vs,buffer);
+              for (unsigned int i = 1; i < vs.size(); i++)
+                y.push_back(atof(vs[i].c_str()));
+              ifs->getline(buffer, BUFF_SIZE);
+              tokenize(vs,buffer);
+              for (unsigned int i = 1; i < vs.size(); i++)
+                z.push_back(atof(vs[i].c_str()));
+              for (unsigned int i = 0; i < freq.size(); i++)
+              {
+                vib.push_back(vector<vector3>());
+                vib[i].push_back(vector3(x[i], y[i], z[i]));
+              }
+              ifs->getline(buffer, BUFF_SIZE);
+              tokenize(vs,buffer);
+            }// while vs.size() > 2
+            for (unsigned int i = 0; i < freq.size(); i++)
+              {
+                if (abs(freq[i]) > 10.0)
+                {
+                   // skip rotational and translational modes
+                   Frequencies.push_back(freq[i]);
+                   Lx.push_back(vib[i]);
+                }// if abs(freq[i]) > 10.0
+              }// for (unsigned int i = 0; i < freq.size(); i++)
+        }// if P.Frequency
+        else if(strstr(buffer,"Projected Infra Red Intensities") != NULL)
+        {
+            ifs->getline(buffer, BUFF_SIZE); // table header
+            ifs->getline(buffer, BUFF_SIZE); // table delimiter
+            ifs->getline(buffer, BUFF_SIZE);
+            tokenize(vs,buffer);
+            while (vs.size() == 7)
+            {
+              if (abs(atof(vs[1].c_str())) > 10.0)
+                 Intensities.push_back(atof(vs[5].c_str()));
+              ifs->getline(buffer, BUFF_SIZE);
+              tokenize(vs,buffer);
+            }
+        } // if "Projected Infra Red Intensities"
+        else if(strstr(buffer, "Task  times  cpu") != NULL)
+        {
+            // End of task
+            break;
+        } // if "Task  times  cpu"
+    }
 
-        for (unsigned int i = 0; i < freq.size(); i++)
-          {
-            if (abs(freq[i]) > 10.0)
-             {
-               // skip rotational and translational modes
-               Frequencies.push_back(freq[i]);
-               Lx.push_back(vib[i]);
-             }// if abs(freq[i]) > 10.0
-          }// for
 
-        ifs->getline(buffer, BUFF_SIZE); // frequency counting
-        ifs->getline(buffer, BUFF_SIZE); // blank line
-        ifs->getline(buffer, BUFF_SIZE); // P.Frequency  etc.
-      }//while P.Frequency
+    if (Frequencies.size() == 0)
+        return NULL;
 
-    if (vibration_data == NULL)
-        vibration_data = new OBVibrationData;
-    else
-      {
-        //Check if read frequencies is not related to supplied OBVibrationData
-        if (vibration_data->GetNumberOfFrequencies() != Frequencies.size())
-            return NULL;
-        vector<double> supplied_frequencies = vibration_data->GetFrequencies();
-        for (unsigned int i = 0;i < supplied_frequencies.size();i++)
-          {
-            if (supplied_frequencies[i] != Frequencies[i])
-                return NULL;
-          }//for
-      } // if !vibration_data
-
-    vibration_data->SetData(Lx, Frequencies, vibration_data->GetIntensities());
+    vibration_data = new OBVibrationData;
+    vibration_data->SetData(Lx, Frequencies, Intensities);
 
     return vibration_data;
+  }
+
+  /////////////////////////////////////////////////////////////////
+  static bool CheckMoleculesEqual(OBMol* mol1, OBMol* mol2)
+  {
+    if (mol1->NumAtoms() != mol2->NumAtoms())
+        return false;
+    for(unsigned int i = 0; i < mol1->NumAtoms();i++)
+        if(mol1->GetAtomById(i)->GetAtomicNum() != mol2->GetAtomById(i)->GetAtomicNum())
+            return false;
+    return true;
+  }
+
+  static void rewind_to_calculation_end(istream *ifs)
+  {
+  char buffer[BUFF_SIZE];
+    while ( (strstr(buffer,"Task  times  cpu") == NULL))
+        if (ifs->getline(buffer,BUFF_SIZE) == NULL)
+            break;
   }
 
   /////////////////////////////////////////////////////////////////
@@ -257,116 +346,87 @@ namespace OpenBabel
     OBMol &mol = *pmol;
     const char* title = pConv->GetTitle();
 
-    //Vibrational data
-    OBVibrationData* vibration_data = NULL;
-    std::vector<double> Intensities;
-
     char buffer[BUFF_SIZE];
     string str;
     double x,y,z;
     OBAtom *atom;
     vector<string> vs;
-    
-    
-    
+    double last_calculated_energy;
+
+    mol.BeginModify();
+
     // Reading inital parameters of calculation such as
     // used theory and calculation type for better
     // recognition futher output
-    string theory;
-    string energy_pattern;
-    CalculationType current_calculation = SinglePoint;
-    while	(ifs.getline(buffer,BUFF_SIZE))
-      {
-        if(strstr(buffer,"NWChem SCF Module") != NULL)
-          {
-            theory = "SCF";
-            break;
-          }// if "SCF Module"
-        else if(strstr(buffer,"NWChem DFT Module") != NULL)
-          {
-            theory = "DFT";
-            break;
-          }// if "DFT Module"
-        else if(strstr(buffer,"NWChem Geometry Optimization") != NULL)
-          {
-            current_calculation = GeometryOptimization;
-          }// if "Geometry Optimization"
-        else if(strstr(buffer,"NWChem Nuclear Hessian and Frequency Analysis") != NULL)
-          {
-            current_calculation = VibrationalAnalysis;
-          }// if "Frequency Analysis"
-        else if(strstr(buffer,"@ String method.") != NULL)
-          {
-            current_calculation = ZTS;
-          }// if "String method"
-        else if(strstr(buffer,"NWChem Property Module") != NULL)
-          {
-            current_calculation = Property;
-          }// if "Property"
-          
-      }//while
-    
-    if (!theory.empty())
+    while	(ifs.getline(buffer,BUFF_SIZE) != NULL)
     {
-        stringstream ss;
-        ss << "Total " << theory << " energy";
-        energy_pattern = ss.str();
-    }
-    // "Task  times  cpu" End marker
-    mol.BeginModify();
-    while	(ifs.getline(buffer,BUFF_SIZE))
-      {
         if(strstr(buffer,"Output coordinates") != NULL)
-          {
-            // mol.EndModify();
-            OBMol* atoms = ReadCoordinates(&ifs);
-            mol.Clear();
-            mol.BeginModify();
-            mol.ReserveAtoms(atoms->NumAtoms());
-            for(unsigned int i = 0;i < atoms->NumAtoms();i++)
-              {
-                OBAtom* atom = atoms->GetAtomById(i);
-                OBAtom* writeatom = mol.NewAtom();
-                writeatom->SetVector(atom->GetVector());
-                writeatom->SetAtomicNum(atom->GetAtomicNum());
-              }
-            delete atoms;
-          } // if "output coordinates"
-        if(strstr(buffer,"(Projected Frequencies expressed in cm-1)") != NULL)
-          {
-            vibration_data = ReadFrequencies(&ifs, vibration_data);
-          } // if "P.Frequency"
-        if(strstr(buffer,"Projected Infra Red Intensities") != NULL)
-          {
-           ifs.getline(buffer, BUFF_SIZE); // table header
-           ifs.getline(buffer, BUFF_SIZE); // table delimiter
-           ifs.getline(buffer, BUFF_SIZE);
-           tokenize(vs,buffer);
-           while (vs.size() == 7) {
-             if (abs(atof(vs[1].c_str())) > 10.0)
-                Intensities.push_back(atof(vs[5].c_str()));
-             ifs.getline(buffer, BUFF_SIZE);
-             tokenize(vs,buffer);
-           }
-         } // if "Projected Infra Red Intensities"
-       if((!energy_pattern.empty()) && (strstr(buffer, energy_pattern.c_str()) != NULL))
-         {
-            tokenize(vs,buffer);
-            mol.SetEnergy(atof(vs[3].c_str()) * HARTREE_TO_KCAL);
-         }// if "Total energy"
-      } // while
+        {
+            // Input coordinates for calculation
+            OBMol* geometry = ReadCoordinates(&ifs);
+            if (pConv->IsOption("f",OBConversion::INOPTIONS))
+            {
+                // If coordinates will be redefined while calculation
+                // in input file and "f" option supplied then overwrite
+                // all previous calculations. Otherwise calculations for
+                // new geometry will be ignored.
+                mol.Clear();
+            }
+            if (mol.NumAtoms() == 0)
+                mol += *geometry;
+            delete geometry;
+        }
+        else if(strstr(buffer,"NWChem Geometry Optimization") != NULL)
+        {
+            OBMol* result = ReadGeometryOptimizationCalculation(&ifs);
+            if (result != NULL)
+            {
+                if (mol.NumAtoms() == 0)
+                {
+                    mol += *result;
+                    delete result;
+                    continue;
+                }
+                else if (!CheckMoleculesEqual(&mol, result))
+                {
+                    delete result;
+                    continue;
+                }
+                unsigned int natoms = result->NumAtoms();
+                vector<double*> conformers;
+                conformers.reserve(result->NumConformers());
+                for(unsigned int i = 0;i < result->NumConformers();i++)
+                {
+                    double * conformer = new double[natoms*3];
+                    memcpy(conformer, result->GetConformer(i),sizeof(double)*3*natoms);
+                    mol.AddConformer(conformer);
+                }
+                vector<double> energies = result->GetEnergies();
+                mol.SetEnergies(energies);
+                last_calculated_energy = energies[energies.size() -1];
+                mol.SetConformer(mol.NumConformers() - 1);
+                for(unsigned int i = 0;i < natoms;i++)
+                    mol.GetAtomById(i)->SetVector(result->GetAtomById(i)->GetVector());
+                delete result;
+            }
+        }// if "Geometry Optimization"
+        else if(strstr(buffer,"NWChem Nuclear Hessian and Frequency Analysis") != NULL)
+        {
+            OBVibrationData* result = ReadFrequencyCalculation(&ifs);
+            if (result != NULL)
+                mol.SetData(result);
+        }// if "Frequency Analysis"
+        // These calculation handlers still not implemented
+        // so we just skip them
+        else if(strstr(buffer,"@ String method.") != NULL)
+            rewind_to_calculation_end(&ifs); 
+        else if(strstr(buffer,"NWChem Property Module") != NULL)
+            rewind_to_calculation_end(&ifs);
+    }//while
 
     if (mol.NumAtoms() == 0) { // e.g., if we're at the end of a file PR#1737209
       mol.EndModify();
       return false;
-    }
-
-    //Attach vibrational data, if there is any, to molecule
-    if(vibration_data != NULL)
-    {
-      if ((Intensities.size() > 0) && (vibration_data->GetNumberOfFrequencies() == Intensities.size()))
-        vibration_data->SetData(vibration_data->GetLx(),vibration_data->GetFrequencies(),Intensities);
-      mol.SetData(vibration_data);
     }
 
     if (!pConv->IsOption("b",OBConversion::INOPTIONS))
@@ -375,6 +435,11 @@ namespace OpenBabel
       mol.PerceiveBondOrders();
 
     mol.EndModify();
+    // EndModify adds new conformer equals to current molecule geometry
+    // so we will just assign last calculated energy to this conformer
+    mol.SetConformer(mol.NumConformers() - 1);
+    mol.SetEnergy(last_calculated_energy);
+
     mol.SetTitle(title);
     return(true);
   }
