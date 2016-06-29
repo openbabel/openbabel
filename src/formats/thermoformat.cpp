21712d4a19d1da9af0fdc77b1382177d7169dfc3
Maciej Wójcikowski <mwojcikowski@users.noreply.github.com>
2016-06-29 13:10:36 +0200

    Merge ifs

--- src/formats/thermoformat.cpp
+++ src/formats/thermoformat.cpp
@@ -165,10 +165,8 @@ bool ThermoFormat::WriteMolecule(OBBase* pOb, OBConversion* pConv)
   }
   ostream &ofs = *pConv->GetOutStream();
   unsigned int i;
-#ifdef _MSC_VER
-#if _MSC_VER < 1900
+#if defined(_MSC_VER) && (_MSC_VER < 1900)
   unsigned oldf = _set_output_format(_TWO_DIGIT_EXPONENT);
-#endif
 #endif
   string formula = pmol->GetSpacedFormula();
   vector<string> toks;
