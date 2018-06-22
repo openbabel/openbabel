6bfdc2e7544f0cf8cdc9f3b224c4fd36798ca5ea
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2018-06-22 16:59:20 +0530

    Update color.h

--- include/color.h
+++ include/color.h
@@ -7,6 +7,17 @@
 extern "C" {
 #include <unistd.h>
 }
+#include <stdio.h>
+ 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_BOLD    "\x1b[1m"
+#define ANSI_COLOR_DIM     "\x1b[2m"
 
 namespace color {
 
@@ -84,4 +95,3 @@ namespace {
     }
 }
 #endif /* ifndef COLOR_H*/
-
