948936c161cf8a120224b532edb570c57ce81d47
Ashutosh Pandey <pandeyashutosh1986@yahoo.com>
2018-08-09 01:20:14 +0530

    Update color.h

--- include/openbabel/color.h
+++ include/openbabel/color.h
@@ -74,7 +74,24 @@ struct streamstate {
     std::string clogstate;
 };
 extern streamstate state;
-bool update(std::ostream &os,int v);
+
+bool update(std::ostream &os,int v){
+  if(&os == &std::cout){
+    std::stringstream s;
+    s << "\e[" << v << "m";
+    state.coutstate = s.str();
+    std::clog << state.clogstate;
+    return true;
+  }
+  else if(&os == &std::clog){
+    std::stringstream s;
+    s << "\e[" << v << "m";
+    state.clogstate = s.str();
+    std::cout << state.coutstate;
+    return true;
+  }
+  return false;
+}
 
 namespace {
     bool isAllowed = false;
