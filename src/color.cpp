54b36f85a83089a7c106bae75c48017feba1e555
Chinmay Garg <32796540+chinmaygarg13@users.noreply.github.com>
2018-08-09 00:19:15 +0530

    Update color.cpp

--- src/color.cpp
+++ src/color.cpp
@@ -3,14 +3,14 @@
 streamstate state = {"","",""};
 
 bool update(std::ostream &os,int v){
-  if(os == std::cout){
+  if(&os == &std::cout){
     std::stringstream s;
     s << "\e[" << v << "m";
     state.coutstate = s.str();
     std::clog << state.clogstate;
     return true;
   }
-  else if(os == std::clog){
+  else if(&os == &std::clog){
     std::stringstream s;
     s << "\e[" << v << "m";
     state.clogstate = s.str();
