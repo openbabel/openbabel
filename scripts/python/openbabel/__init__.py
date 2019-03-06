0c8c4d8d4e27faecfc75a7258c015255aa4431cf
Noel O'Boyle <baoilleach@gmail.com>
2019-03-06 15:42:45 +0000

    Use Matt Swain's __init__.py hack to support 'import openbabel' but mark as deprecated

--- scripts/python/openbabel/__init__.py
+++ scripts/python/openbabel/__init__.py
@@ -1,1 +1,17 @@
-# Hint: use "from openbabel import openbabel" or "from openbabel import pybel"
+import sys
+import warnings
+from . import openbabel
+
+_thismodule = sys.modules[__name__]
+
+class OBProxy(object):
+    def __getattr__(self, name):
+        if hasattr(_thismodule, name):
+            return getattr(_thismodule, name)
+        elif hasattr(openbabel, name):
+            warnings.warn('"import openbabel" is deprecated, instead use "from openbabel import openbabel"')
+            return getattr(openbabel, name)
+        else:
+            raise AttributeError
+
+sys.modules[__name__] = OBProxy()
