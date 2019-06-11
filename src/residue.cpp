7a1e761e4f43e989ea3b1c0b4eeb50738edfe64e
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-06-11 14:35:35 +0100

    indentation

--- src/residue.cpp
+++ src/residue.cpp
@@ -841,7 +841,7 @@ _insertioncode=0;
     _atomid   = src._atomid;
     _hetatm   = src._hetatm;
     _sernum   = src._sernum;
-_insertioncode=src._insertioncode;
+    _insertioncode=src._insertioncode;
 
   }
 
@@ -872,7 +872,7 @@ _insertioncode=src._insertioncode;
         _atomid   = src._atomid;
         _hetatm   = src._hetatm;
         _sernum   = src._sernum;
-   _insertioncode = src._insertioncode;
+        _insertioncode = src._insertioncode;
       }
 
     return(*this);
