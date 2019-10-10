3f7a07f7e40afa3fcce3bf9014f3c3a8ceca0da5
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2019-10-10 18:12:00 +0900

    Do not hard-code Open Babel major version for include directory

--- openbabel-3.pc.cmake
+++ openbabel-3.pc.cmake
@@ -2,7 +2,7 @@ prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
 libdir=${exec_prefix}/@LIB_INSTALL_DIR@
 includedir=${prefix}/include
-pkgincludedir=${includedir}/openbabel-2.0
+pkgincludedir=${includedir}/openbabel@BABEL_MAJ_VER@
 
 Name: Open Babel library
 Description: libopenbabel
