2a0f345262bbf9b71ca44373a02abde41b697583
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-05-12 11:58:42 +0900

    Comment out unused parameters

--- src/formats/pointcloudformat.cpp
+++ src/formats/pointcloudformat.cpp
@@ -110,7 +110,7 @@ namespace OpenBabel
       }
 
       /// Skip to object: used for multi-object file formats.
-      int SkipObjects( int n, OpenBabel::OBConversion* pConv ) override { return 0; }
+      int SkipObjects( int /*n*/, OpenBabel::OBConversion* /*pConv*/ ) override { return 0; }
 
       /// Read: always return false.
       bool ReadMolecule(OpenBabel::OBBase*, OpenBabel::OBConversion*) override
