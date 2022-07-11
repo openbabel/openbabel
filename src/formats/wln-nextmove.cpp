2579c5d7bc4cac6f884e7c4ece7d6e904b39d719
Michael Blakey <78424742+Mblakey@users.noreply.github.com>
2022-07-11 19:14:09 +0100

    fixed case switch extension

--- src/formats/wln-nextmove.cpp
+++ src/formats/wln-nextmove.cpp
@@ -64,9 +64,6 @@ void NMOBMolDestroyBond(OpenBabel::OBMol* mol,
                         OpenBabel::OBAtom* beg,
                         OpenBabel::OBAtom* end)
 {
-    if (!mol->GetBond(beg->GetIdx(), end->GetIdx()))
-        std::cout << "No Bond Found" << std::endl;
-
     OpenBabel::OBBond* destroy = mol->GetBond(beg->GetIdx(),end->GetIdx());
     mol->DestroyBond(destroy);
 }
@@ -918,7 +915,50 @@ struct WLNParser {
                     ptr++;}
                 break;
 
-            case 9 ... 50:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
                 if (macro){
                     macro=false;
                     break;}
