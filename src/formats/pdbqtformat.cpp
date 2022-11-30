fe92ea6adbfcac2d1d8a77bd104ce387a389f6f5
fredrikw <fredrik@wallner.nu>
2022-11-30 12:39:45 +0100

    Add missing import

--- src/formats/pdbqtformat.cpp
+++ src/formats/pdbqtformat.cpp
@@ -22,6 +22,7 @@ GNU General Public License for more details.
 
 #include <openbabel/babelconfig.h>
 #include <openbabel/obmolecformat.h>
+#include <openbabel/obfunctions.h>
 #include <openbabel/mol.h>
 #include <openbabel/atom.h>
 #include <openbabel/elements.h>
