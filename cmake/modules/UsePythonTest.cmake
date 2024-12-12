e3cb5adca835b1960b5db4b741c381d78589f997
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-12 10:48:59 +0900

    test: refactor ADD_PYTHON_TEST to directly invoke python

--- cmake/modules/UsePythonTest.cmake
+++ cmake/modules/UsePythonTest.cmake
@@ -1,12 +1,3 @@
-# Add a python test from a python file
-# One cannot simply do:
-# SET(ENV{PYTHONPATH} ${LIBRARY_OUTPUT_PATH})
-# SET(my_test "from test_mymodule import *\;test_mymodule()")
-# ADD_TEST(PYTHON-TEST-MYMODULE  python -c ${my_test})
-# Since cmake is only transmitting the ADD_TEST line to ctest thus you are loosing
-# the env var. The only way to store the env var is to physically write in the cmake script
-# whatever PYTHONPATH you want and then add the test as 'cmake -P python_test.cmake'
-# 
 # Usage:
 # SET_SOURCE_FILES_PROPERTIES(test.py PROPERTIES PYTHONPATH
 #   "${LIBRARY_OUTPUT_PATH}:${VTK_DIR}")
@@ -43,31 +34,8 @@ MACRO(ADD_PYTHON_TEST TESTNAME FILENAME)
   GET_SOURCE_FILE_PROPERTY(ob_libdir ${FILENAME} BABEL_LIBDIR)
   GET_SOURCE_FILE_PROPERTY(ob_datadir ${FILENAME} BABEL_DATADIR)
   STRING(REGEX REPLACE ";" " " wo_semicolumn "${ARGN}")
-  FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${TESTNAME}.cmake
-"
-  SET(ENV{PYTHONPATH} ${pyenv})
-  SET(ENV{LD_LIBRARY_PATH} ${pyenv}:\$ENV{LD_LIBRARY_PATH})
-  SET(ENV{BABEL_LIBDIR} ${ob_libdir})
-  SET(ENV{BABEL_DATADIR} ${ob_datadir})
-  MESSAGE(\"${pyenv}\")
-  EXECUTE_PROCESS(
-  	COMMAND ${PYTHON_EXECUTABLE} ${loc} ${wo_semicolumn}
-  	#WORKING_DIRECTORY @LIBRARY_OUTPUT_PATH@
-  	RESULT_VARIABLE import_res
-  	OUTPUT_VARIABLE import_output
-  	ERROR_VARIABLE  import_output
-  )
-  
-  # Pass the output back to ctest
-  IF(import_output)
-    MESSAGE(\${import_output})
-  ENDIF(import_output)
-  IF(import_res)
-    MESSAGE(SEND_ERROR \${import_res})
-  ENDIF(import_res)
-"
-)
-  ADD_TEST(${TESTNAME} ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/${TESTNAME}.cmake)
+  ADD_TEST(NAME ${TESTNAME} COMMAND ${PYTHON_EXECUTABLE} ${loc} ${wo_semicolumn})
+  SET_TESTS_PROPERTIES(${TESTNAME} PROPERTIES ENVIRONMENT "PYTHONPATH=${pyenv};LD_LIBRARY_PATH=${pyenv}:$ENV{LD_LIBRARY_PATH};BABEL_LIBDIR=${ob_libdir};BABEL_DATADIR=${ob_datadir}")
 ENDMACRO(ADD_PYTHON_TEST)
 
 # Byte compile recursively a directory (DIRNAME)
