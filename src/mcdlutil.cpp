e18c41a1da7d576df45ef34e31f85af933378e34
crivella <davidecrivella@yahoo.it>
2026-02-07 03:30:07 +0100

    Fix non initialized ac array can cause undefined behavior

--- src/mcdlutil.cpp
+++ src/mcdlutil.cpp
@@ -1627,6 +1627,9 @@ namespace OpenBabel {
     for (i=0; i<nAtoms(); i++) {
       getAtom(i)->nb=0;
       getAtom(i)->currvalence=0;
+      for (int j=0; j<CONNMAX; j++) {
+        getAtom(i)->ac[j]=0;
+      }
     };
     for (i=0; i<nBonds(); i++) {
       n1=getBond(i)->at[0]; n2=getBond(i)->at[1];
